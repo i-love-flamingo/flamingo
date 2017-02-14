2a847a74c10dd80d7c344a6bdf5dcf407d6b5587
Bastian Ike <bastian.ike@aoe.com>
2017-02-14 12:51:39 +0100

    remove webpack proxy

--- core/core/app/web/prefix_router.go
+++ core/core/app/web/prefix_router.go
@@ -1,8 +1,6 @@
 package web
 
 import (
-	"io"
-	"net"
 	"net/http"
 	"path"
 	"strings"
@@ -11,21 +9,6 @@ import (
 type PrefixRouter map[string]http.Handler
 
 func (pr PrefixRouter) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	if req.RequestURI == "/__webpack_hmr" {
-		h := w.(http.Hijacker)
-		_, buf, _ := h.Hijack()
-
-		c, _ := net.Dial("tcp", "localhost:1337")
-		c.Write([]byte("GET /__webpack_hmr HTTP/1.1\r\nHost: localhost\r\n\r\n"))
-
-		go func() {
-			io.Copy(buf, c)
-		}()
-		io.Copy(c, buf)
-
-		return
-	}
-
 	host := req.Host
 	if strings.Index(host, ":") > -1 {
 		host = strings.Split(host, ":")[0]

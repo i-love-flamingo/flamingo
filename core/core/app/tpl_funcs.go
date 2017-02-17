8373c779a106d35e664e76b8140f723c4557cdb4
Bastian Ike <bastian.ike@aoe.com>
2017-02-17 14:18:12 +0100

    url template function

--- core/core/app/tpl_funcs.go
+++ core/core/app/tpl_funcs.go
@@ -1,6 +1,9 @@
 package app
 
-import "flamingo/core/core/app/web"
+import (
+	"flamingo/core/core/app/web"
+	"html/template"
+)
 
 type GetFunc struct {
 	App *App `inject:""`
@@ -15,5 +18,23 @@ func (g *GetFunc) Func() interface{} {
 		return func(what string) interface{} {
 			return g.App.Get(what, ctx)
 		}
+	}
+}
+
+type UrlFunc struct {
+	App *App `inject:""`
+}
+
+func (_ UrlFunc) Name() string {
+	return "url"
+}
+
+func (u *UrlFunc) Func() interface{} {
+	return func(where string, params map[string]string) template.URL {
+		p := make([]string, len(params)*2)
+		for k, v := range params {
+			p = append(p, k, v)
+		}
+		return template.URL(u.App.Url(where, p...).String())
 	}
 }

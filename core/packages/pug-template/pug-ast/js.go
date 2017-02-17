d88ce4843dd0b5cb5d6df584d04b8329effb8700
Bastian Ike <bastian.ike@aoe.com>
2017-02-17 17:46:12 +0100

    Flamingo: rework js parser

--- core/packages/pug-template/pug-ast/js.go
+++ core/packages/pug-template/pug-ast/js.go
@@ -9,9 +9,55 @@ import (
 	"github.com/robertkrimen/otto/token"
 )
 
+var (
+	known map[string]bool
+
+	ops = map[token.Token]string{
+		token.PLUS:      "__op__add",   // +
+		token.MINUS:     "__op__sub",   // -
+		token.MULTIPLY:  "__op__mul",   // *
+		token.SLASH:     "__op__slash", // /
+		token.REMAINDER: "__op__mod",   // %
+
+		token.AND:                  "__op__b_and",     // &
+		token.OR:                   "__op__b_or",      // |
+		token.EXCLUSIVE_OR:         "__op__b_xor",     // ^
+		token.SHIFT_LEFT:           "__op__b_sleft",   // <<
+		token.SHIFT_RIGHT:          "__op__b_sright",  // >>
+		token.UNSIGNED_SHIFT_RIGHT: "__op__b_usright", // >>>
+		token.AND_NOT:              "__op__b_andnot",  // &^
+
+		token.LOGICAL_AND: "and",       // &&
+		token.LOGICAL_OR:  "or",        // ||
+		token.INCREMENT:   "__op__inc", // ++
+		token.DECREMENT:   "__op__dec", // --
+
+		token.EQUAL:        "eq",  // ==
+		token.STRICT_EQUAL: "eq",  // ===
+		token.LESS:         "lt",  // <
+		token.GREATER:      "gt",  // >
+		token.ASSIGN:       "=",   // =
+		token.NOT:          "not", // !
+
+		token.BITWISE_NOT: "__op__bitnot", // ~
+
+		token.NOT_EQUAL:        "neq", // !=
+		token.STRICT_NOT_EQUAL: "neq", // !==
+		token.LESS_OR_EQUAL:    "lte", // <=
+		token.GREATER_OR_EQUAL: "gte", // >=
+
+		token.DELETE: "delete",
+	}
+)
+
+func init() {
+	known = make(map[string]bool)
+	known["attributes"] = true
+}
+
+// JsExpr transforms a javascript expression to go code
 func JsExpr(expr string, wrap, rawcode bool) string {
 	var finalexpr string
-
 	var stmtlist []ast.Statement
 
 	if rawcode {
@@ -31,15 +77,19 @@ func JsExpr(expr string, wrap, rawcode bool) string {
 	}
 
 	for _, stmt := range stmtlist {
-		if expr, ok := stmt.(*ast.ExpressionStatement); ok {
+		switch expr := stmt.(type) {
+		case *ast.ExpressionStatement:
 			finalexpr += renderExpression(expr.Expression, wrap, true)
-		} else if expr, ok := stmt.(*ast.VariableStatement); ok {
+
+		case *ast.VariableStatement:
 			for _, v := range expr.List {
 				finalexpr += renderExpression(v, wrap, true)
 			}
-		} else if expr, ok := stmt.(*ast.ReturnStatement); ok {
+
+		case *ast.ReturnStatement:
 			finalexpr += renderExpression(expr.Argument, wrap, true)
-		} else {
+
+		default:
 			fmt.Printf("%#v\n", stmt)
 			panic("unknown expression")
 		}
@@ -48,74 +98,26 @@ func JsExpr(expr string, wrap, rawcode bool) string {
 	return finalexpr
 }
 
-var ops = map[token.Token]string{
-	token.PLUS:      "__op__add",   // +
-	token.MINUS:     "__op__sub",   // -
-	token.MULTIPLY:  "__op__mul",   // *
-	token.SLASH:     "__op__slash", // /
-	token.REMAINDER: "__op__mod",   // %
-
-	token.AND:                  "__op__b_and",     // &
-	token.OR:                   "__op__b_or",      // |
-	token.EXCLUSIVE_OR:         "__op__b_xor",     // ^
-	token.SHIFT_LEFT:           "__op__b_sleft",   // <<
-	token.SHIFT_RIGHT:          "__op__b_sright",  // >>
-	token.UNSIGNED_SHIFT_RIGHT: "__op__b_usright", // >>>
-	token.AND_NOT:              "__op__b_andnot",  // &^
-
-	token.LOGICAL_AND: "and",       // &&
-	token.LOGICAL_OR:  "or",        // ||
-	token.INCREMENT:   "__op__inc", // ++
-	token.DECREMENT:   "__op__dec", // --
-
-	token.EQUAL:        "eq",  // ==
-	token.STRICT_EQUAL: "eq",  // ===
-	token.LESS:         "lt",  // <
-	token.GREATER:      "gt",  // >
-	token.ASSIGN:       "=",   // =
-	token.NOT:          "not", // !
-
-	token.BITWISE_NOT: "__op__bitnot", // ~
-
-	token.NOT_EQUAL:        "neq", // !=
-	token.STRICT_NOT_EQUAL: "neq", // !==
-	token.LESS_OR_EQUAL:    "lte", // <=
-	token.GREATER_OR_EQUAL: "gte", // >=
-
-	token.DELETE: "delete",
-}
-
-var known map[string]bool
-
-func init() {
-	known = make(map[string]bool)
-	known["attributes"] = true
-}
-
-// a#{b}c
-// start 3
-// index 4
-
-func interpolate(s string) string {
+func interpolate(input string) string {
 	index := 1
 	start := 0
 
-	for index < len(s) {
+	for index < len(input) {
 		switch {
-		case s[index] == '\\':
+		case input[index] == '\\':
 
-		case s[index] == '{' && s[index-1] == '$':
+		case input[index] == '{' && input[index-1] == '$':
 			start = index + 1
 
-		case s[index] == '}' && start != 0:
-			ss := JsExpr(s[start:index], false, false)
-			s = s[:start-2] + `" ` + ss + ` "` + s[index+1:]
-			index = start + len(ss)
+		case input[index] == '}' && start != 0:
+			substring := JsExpr(input[start:index], false, false)
+			input = input[:start-2] + `" ` + substring + ` "` + input[index+1:]
+			index = start + len(substring)
 			start = 0
 		}
 		index++
 	}
-	return s
+	return input
 }
 
 func renderExpression(expr ast.Expression, wrap bool, dot bool) string {
@@ -123,129 +125,158 @@ func renderExpression(expr ast.Expression, wrap bool, dot bool) string {
 		return ""
 	}
 
-	var finalexpr string
+	var result string
 
-	if str, ok := expr.(*ast.StringLiteral); ok {
-		if strings.Index(str.Value, "${") >= 0 {
-			finalexpr = `(s "` + interpolate(str.Value) + `")`
-		} else {
-			finalexpr = `"` + str.Value + `"`
-		}
-
-		if wrap {
-			lf := len(finalexpr)
-			if finalexpr[0] == '"' && finalexpr[lf-1] == '"' {
-				return finalexpr[1 : lf-1]
-			}
-			finalexpr = `{{` + finalexpr + `}}`
-		}
-
-	} else if identifier, ok := expr.(*ast.Identifier); ok {
-		if known[identifier.Name] {
-			finalexpr += `$`
+	switch expr := expr.(type) {
+	// Identifier: usually a variable name
+	case *ast.Identifier:
+		if known[expr.Name] {
+			result += `$`
 		} else if dot {
-			finalexpr += `.`
+			result += `.`
 		}
-		finalexpr += identifier.Name
+		result += expr.Name
 		if wrap {
-			finalexpr = `{{` + finalexpr + ` | raw}}`
+			result = `{{` + result + ` | raw}}`
 		}
-	} else if de, ok := expr.(*ast.DotExpression); ok {
-		finalexpr += renderExpression(de.Left, false, true) + "." + renderExpression(de.Identifier, false, true)[1:]
+
+	// StringLiteral: "test" or 'test' or `test`
+	case *ast.StringLiteral:
+		if strings.Index(expr.Value, "${") >= 0 {
+			result = `(s "` + interpolate(expr.Value) + `")`
+			if wrap {
+				result = `{{` + result + `}}`
+			}
+		} else {
+			if wrap {
+				result = expr.Value
+			} else {
+				result = `"` + expr.Value + `"`
+			}
+		}
+
+	// NumberLiteral: 1 or 1.5
+	case *ast.NumberLiteral:
+		result = fmt.Sprintf("%v", expr.Value)
+
+	// ArrayLiteral: [1, 2, 3]
+	case *ast.ArrayLiteral:
+		result += `(__op__array`
+		for _, e := range expr.Value {
+			result += ` ` + renderExpression(e, false, true)
+		}
+		result += `)`
 		if wrap {
-			finalexpr = `{{` + finalexpr + ` | raw}}`
+			result = `{{` + result + `}}`
 		}
-	} else if conditional, ok := expr.(*ast.ConditionalExpression); ok {
-		finalexpr = `{{if ` + renderExpression(conditional.Test, false, true) + ` }}`
-		finalexpr += renderExpression(conditional.Consequent, true, true)
-		if renderExpression(conditional.Alternate, true, true) != "" {
-			finalexpr += `{{else}}`
-			finalexpr += renderExpression(conditional.Alternate, true, true)
+
+	// BooleanLiteral: true or false
+	case *ast.BooleanLiteral:
+		result = expr.Literal
+
+	// ObjectLiteral: {"key": "value", "key2": something}
+	case *ast.ObjectLiteral:
+		result = `(__op__map `
+		for _, o := range expr.Value {
+			result += ` "` + o.Key + `" ` + renderExpression(o.Value, false, true)
 		}
-		finalexpr += `{{end}}`
-	} else if be, ok := expr.(*ast.BinaryExpression); ok {
-		finalexpr = fmt.Sprintf(
-			`(%s %s %s)`,
-			ops[be.Operator],
-			renderExpression(be.Left, false, true),
-			renderExpression(be.Right, false, true))
-		if wrap {
-			finalexpr = `{{` + finalexpr + `}}`
-		}
-	} else if ce, ok := expr.(*ast.CallExpression); ok {
-		finalexpr = `(` + renderExpression(ce.Callee, false, false)
-		for _, c := range ce.ArgumentList {
-			finalexpr += ` ` + renderExpression(c, false, true)
-		}
-		finalexpr += `)`
-		if wrap {
-			finalexpr = `{{` + finalexpr + `}}`
-		}
-	} else if ae, ok := expr.(*ast.AssignExpression); ok {
-		n := renderExpression(ae.Left, false, false)
-		n = strings.TrimLeft(n, "$")
-		finalexpr = fmt.Sprintf(`$%s :%s %s`,
-			n,
-			ops[ae.Operator],
-			renderExpression(ae.Right, false, true))
-		known[n] = true
-		if wrap {
-			finalexpr = `{{` + finalexpr + `}}`
-		}
-	} else if nl, ok := expr.(*ast.NumberLiteral); ok {
-		finalexpr = fmt.Sprintf("%v", nl.Value)
-	} else if _, ok := expr.(*ast.FunctionLiteral); ok {
-		finalexpr = `FUNC//CLOSURE//`
-	} else if al, ok := expr.(*ast.ArrayLiteral); ok {
-		finalexpr += `(__op__array`
-		for _, e := range al.Value {
-			finalexpr += ` ` + renderExpression(e, false, true)
-		}
-		finalexpr += `)`
-		if wrap {
-			finalexpr = `{{` + finalexpr + `}}`
-		}
-	} else if bl, ok := expr.(*ast.BooleanLiteral); ok {
-		finalexpr = bl.Literal
-	} else if ve, ok := expr.(*ast.VariableExpression); ok {
-		n := ve.Name
-		n = strings.TrimLeft(n, "$")
-		finalexpr = `$` + n + ` := ` + renderExpression(ve.Initializer, false, true)
-		known[n] = true
-		if wrap {
-			finalexpr = `{{` + finalexpr + `}}`
-		}
-	} else if ol, ok := expr.(*ast.ObjectLiteral); ok {
-		finalexpr = `(__op__map `
-		for _, o := range ol.Value {
-			finalexpr += ` "` + o.Key + `" ` + renderExpression(o.Value, false, true)
-		}
-		finalexpr += `)`
-	} else if _, ok := expr.(*ast.NullLiteral); ok {
-		finalexpr = ``
+		result += `)`
+
+	// NullLiteral: null
+	case *ast.NullLiteral:
+		result = ``
 		if wrap {
 			return `{{null}}`
 		}
-	} else if se, ok := expr.(*ast.SequenceExpression); ok {
-		finalexpr = `(__op__array `
-		for _, s := range se.Sequence {
-			finalexpr += ` ` + renderExpression(s, false, true)
-		}
-		finalexpr += `)`
-	} else if be, ok := expr.(*ast.BracketExpression); ok {
-		finalexpr += `(index ` + renderExpression(be.Left, false, true) + ` ` + renderExpression(be.Member, false, true) + `)`
+
+	// DotExpression: left.right
+	case *ast.DotExpression:
+		result += renderExpression(expr.Left, false, true) + "." + renderExpression(expr.Identifier, false, true)[1:]
 		if wrap {
-			finalexpr = `{{` + finalexpr + `}}`
+			result = `{{` + result + ` | raw}}`
 		}
-	} else if ue, ok := expr.(*ast.UnaryExpression); ok {
-		finalexpr += `(` + ops[ue.Operator] + ` ` + renderExpression(ue.Operand, false, true) + `)`
+
+	// ConditionalExpression: if (something) { ... }
+	case *ast.ConditionalExpression:
+		result = `{{if ` + renderExpression(expr.Test, false, true) + ` }}`
+		result += renderExpression(expr.Consequent, true, true)
+		if renderExpression(expr.Alternate, true, true) != "" {
+			result += `{{else}}`
+			result += renderExpression(expr.Alternate, true, true)
+		}
+		result += `{{end}}`
+
+	// BinaryExpression:  left binary-operator right, 1 & 2, 0xff ^ 0x01
+	case *ast.BinaryExpression:
+		result = fmt.Sprintf(
+			`(%s %s %s)`,
+			ops[expr.Operator],
+			renderExpression(expr.Left, false, true),
+			renderExpression(expr.Right, false, true))
 		if wrap {
-			finalexpr = `{{` + finalexpr + `}}`
+			result = `{{` + result + `}}`
 		}
-	} else {
+
+	// CallExpression: calls a function (Callee) with arguments, e.g. url("target", "arg1", 1)
+	case *ast.CallExpression:
+		result = `(` + renderExpression(expr.Callee, false, false)
+		for _, c := range expr.ArgumentList {
+			result += ` ` + renderExpression(c, false, true)
+		}
+		result += `)`
+		if wrap {
+			result = `{{` + result + `}}`
+		}
+
+	// AssignExpression: assigns something to a variable: foo = ...
+	case *ast.AssignExpression:
+		n := renderExpression(expr.Left, false, false)
+		n = strings.TrimLeft(n, "$")
+		result = fmt.Sprintf(`$%s :%s %s`,
+			n,
+			ops[expr.Operator],
+			renderExpression(expr.Right, false, true))
+		known[n] = true
+		if wrap {
+			result = `{{` + result + `}}`
+		}
+
+	// VariableExpression: creates a new variable, var foo = 1
+	case *ast.VariableExpression:
+		n := expr.Name
+		n = strings.TrimLeft(n, "$")
+		result = `$` + n + ` := ` + renderExpression(expr.Initializer, false, true)
+		known[n] = true
+		if wrap {
+			result = `{{` + result + `}}`
+		}
+
+	// SequenceExpression, just like ArrayLiteral
+	case *ast.SequenceExpression:
+		result = `(__op__array `
+		for _, s := range expr.Sequence {
+			result += ` ` + renderExpression(s, false, true)
+		}
+		result += `)`
+
+	// BracketExpression: access of array/object members, such ass something[1] or foo[bar]
+	case *ast.BracketExpression:
+		result += `(index ` + renderExpression(expr.Left, false, true) + ` ` + renderExpression(expr.Member, false, true) + `)`
+		if wrap {
+			result = `{{` + result + `}}`
+		}
+
+	// UnaryExpression: an operation on an operand, such as delete foo[bar]
+	case *ast.UnaryExpression:
+		result += `(` + ops[expr.Operator] + ` ` + renderExpression(expr.Operand, false, true) + `)`
+		if wrap {
+			result = `{{` + result + `}}`
+		}
+
+	default:
 		fmt.Printf("%#v\n", expr)
 		panic("unknown expression")
 	}
 
-	return finalexpr
+	return result
 }
---
+++ core/packages/pug-template/pug-ast/js_test.go
@@ -0,0 +1,25 @@
+package node
+
+import "testing"
+
+type Case struct {
+	Test, Expected string
+	Raw            bool
+}
+
+var cases = []Case{
+	{"var a = 1", "$a := 1", true},
+	{"1 + 2", "(__op__add 1 2)", false},
+	{"[1, 2, 3, 4, 5]", "(__op__array 1 2 3 4 5)", false},
+	{`{"key": "value", "key2": [1, 2, 3]}`, `(__op__map  "key" "value" "key2" (__op__array 1 2 3))`, false},
+	{`Func(1, 2, 3)`, `(Func 1 2 3)`, false},
+}
+
+func TestJsExpr(t *testing.T) {
+	for _, c := range cases {
+		res := JsExpr(c.Test, false, c.Raw)
+		if res != c.Expected {
+			t.Errorf("%s: %s != %s", c.Test, res, c.Expected)
+		}
+	}
+}

dc76ced026ab96d46c2dc7562b7481c1def5ab57
Bastian Ike <bastian.ike@aoe.com>
2017-02-17 15:06:24 +0100

    fix url func

--- core/core/app/tpl_funcs.go
+++ core/core/app/tpl_funcs.go
@@ -30,10 +30,10 @@ func (_ UrlFunc) Name() string {
 }
 
 func (u *UrlFunc) Func() interface{} {
-	return func(where string, params map[string]string) template.URL {
+	return func(where string, params map[interface{}]interface{}) template.URL {
 		p := make([]string, len(params)*2)
 		for k, v := range params {
-			p = append(p, k, v)
+			p = append(p, k.(string), v.(string))
 		}
 		return template.URL(u.App.Url(where, p...).String())
 	}

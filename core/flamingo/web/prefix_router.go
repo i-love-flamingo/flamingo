35b8d981c7f5dd4bedb7a3e2bb1a9d4057d1ec42
Bastian Ike <bastian.ike@aoe.com>
2017-02-21 17:06:16 +0100

    Flamingo: default router

--- core/flamingo/web/prefix_router.go
+++ core/flamingo/web/prefix_router.go
@@ -1,29 +1,49 @@
 package web
 
 import (
-	"io"
 	"net/http"
 	"net/url"
 	"path"
 	"strings"
 )
 
-type PrefixRouter map[string]http.Handler
-
-func (pr PrefixRouter) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	if strings.HasPrefix(req.RequestURI, "/assets/") {
-		if r, e := http.Get("http://localhost:1337" + req.RequestURI); e == nil {
-			io.Copy(w, r.Body)
-			return
-		}
+type (
+	FrontRouter struct {
+		router   map[string]http.Handler
+		fallback http.Handler
 	}
+)
+
+func NewFrontRouter() *FrontRouter {
+	return &FrontRouter{
+		router: make(map[string]http.Handler),
+	}
+}
+
+func (fr *FrontRouter) Add(prefix string, handler http.Handler) {
+	fr.router[prefix] = handler
+}
+
+func (fr *FrontRouter) Default(handler http.Handler) {
+	fr.fallback = handler
+}
+
+func (fr *FrontRouter) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	/*
+		if strings.HasPrefix(req.RequestURI, "/assets/") {
+			if r, e := http.Get("http://localhost:1337" + req.RequestURI); e == nil {
+				io.Copy(w, r.Body)
+				return
+			}
+		}
+	*/
 
 	host := req.Host
 	if strings.Index(host, ":") > -1 {
 		host = strings.Split(host, ":")[0]
 	}
 	test := path.Join(host, req.RequestURI)
-	for prefix, router := range pr {
+	for prefix, router := range fr.router {
 		if strings.HasPrefix(test, prefix) {
 			req.URL, _ = url.Parse(test[len(prefix):])
 			if req.URL.Path == "" {
@@ -35,7 +55,7 @@ func (pr PrefixRouter) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	}
 
 	test = req.RequestURI
-	for prefix, router := range pr {
+	for prefix, router := range fr.router {
 		if strings.HasPrefix(test, prefix) {
 			req.URL, _ = url.Parse(test[len(prefix):])
 			if req.URL.Path == "" {
@@ -46,6 +66,9 @@ func (pr PrefixRouter) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 		}
 	}
 
-	w.WriteHeader(404)
-	//panic(test + " not routable")
+	if fr.fallback != nil {
+		fr.fallback.ServeHTTP(w, req)
+	} else {
+		w.WriteHeader(404)
+	}
 }

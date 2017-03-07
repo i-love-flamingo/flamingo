9cfbae4573a5b61012972f69444fdb1d90230bd7
Joachim Adomeit <joachim.adomeit@aoe.com>
2017-03-07 18:43:54 +0100

    Testing Data, Validation http status code before setting response to it

--- core/flamingo/web/responder/error.go
+++ core/flamingo/web/responder/error.go
@@ -32,7 +32,14 @@ func (r *ErrorAware) RenderError(context web.Context, error models.AppError) *we
 		data,
 	)
 
-	response.Status = error.Code
+	/*
+		Response.Status only modified if error.Code is a valid HTTP Status Code
+		otherwise 200 is kept from the default response as its probably a proprietary
+		error code (which is also ok, just for display)
+	*/
+	if error.Code > 99 && error.Code < 1000 {
+		response.Status = error.Code
+	}
 
 	return response
 }

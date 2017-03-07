3e21f345155303e58ce2c288357721f95472fce3
Joachim Adomeit <joachim.adomeit@aoe.com>
2017-03-07 14:56:42 +0100

    Adding Configuration for URL, Error Handling, Output Mapping

--- core/flamingo/web/responder/error.go
+++ core/flamingo/web/responder/error.go
@@ -6,10 +6,7 @@ import (
 )
 
 type ErrorAware struct {
-	*RenderAware `inject:""`
-}
-
-type ErrorAwareDebug struct {
+	DebugMode    string `inject:"param:debug.mode"`
 	*RenderAware `inject:""`
 }
 
@@ -22,28 +19,12 @@ func (r *ErrorAware) RenderError(context web.Context, error models.AppError) *we
 	tpl := "pages/error"
 
 	data := ErrorViewData{}
-
-	// Drop Message, should not be shown in public/prod env
-	data.Error.Message = ""
-
 	data.Error = error
 
-	response := r.RenderAware.Render(
-		context,
-		tpl,
-		data,
-	)
-
-	return response
-}
-
-// Render returns a web.ContentResponse with status 200 and ContentType text/html
-func (r *ErrorAwareDebug) RenderError(context web.Context, error models.AppError) *web.ContentResponse {
-	tpl := "pages/error"
-
-	data := ErrorViewData{}
-
-	data.Error = error
+	if r.DebugMode == "0" {
+		// Drop Message, should not be shown in public/prod env
+		data.Error.Message = ""
+	}
 
 	response := r.RenderAware.Render(
 		context,

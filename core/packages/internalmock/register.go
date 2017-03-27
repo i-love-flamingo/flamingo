646102e540d9e7dae9a5d2e9e5882e2aad135ebe
Mathias Schopmans <mathias.schopmans@aoe.com>
2017-03-27 17:17:56 +0200

    Add internalmock for brands

--- core/packages/internalmock/register.go
+++ core/packages/internalmock/register.go
@@ -2,10 +2,12 @@ package internalmock
 
 import (
 	di "flamingo/core/flamingo/dependencyinjection"
+	"flamingo/core/packages/internalmock/brand"
 	"flamingo/core/packages/internalmock/product"
 )
 
 // Register Services for internalmock package
 func Register(c *di.Container) {
+	c.Register(new(brand.BrandService))
 	c.Register(new(product.ProductService))
 }

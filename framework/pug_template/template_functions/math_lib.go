76c894a0c2308475c0bc20d5f8abc9cb11a0a855
Mathias Schopmans <mathias.schopmans@aoe.com>
2017-05-11 09:43:11 +0200

    Add basic Math.min and Math.max template function

--- framework/pug_template/template_functions/math_lib.go
+++ framework/pug_template/template_functions/math_lib.go
@@ -32,3 +32,29 @@ func (m Math) Ceil(x interface{}) int64 {
 	}
 	return int64(math.Ceil(x.(float64)))
 }
+
+// Min gets the minimum value
+func (m Math) Min(x... interface{}) (res int64) {
+	for _, v := range x {
+		if reflect.TypeOf(v).Kind() == reflect.Int {
+			v = reflect.ValueOf(v).Int()
+		}
+		if v.(int64) < res {
+			res = v.(int64)
+		}
+	}
+	return
+}
+
+// Max gets the maximum value
+func (m Math) Max(x... interface{}) (res int64) {
+	for _, v := range x {
+		if reflect.TypeOf(v).Kind() == reflect.Int {
+			v = reflect.ValueOf(v).Int()
+		}
+		if v.(int64) > res {
+			res = v.(int64)
+		}
+	}
+	return
+}

365a6c00461db49c7559c5f53194ca8aa46e2a2b
Michael Klapper <michael.klapper@aoe.com>
2017-09-20 09:29:06 +0200

    Refactor test for searchperience

--- om3/searchperience/specs/productservice_test.go
+++ om3/searchperience/specs/productservice_test.go
@@ -21,31 +21,18 @@ func TestProductserviceCanGetSimpleProduct(t *testing.T) {
 			UponReceiving("A request to a simple test product").
 			WithRequest(dsl.Request{
 				Method: "GET",
-				Path:   "/product/testen_USecommerce",
+				Path:   "/product/ff97c10c6e69a71d80cb39510830b2c5_simple-en_EN-onlinestore",
 			}).
 			WillRespondWith(dsl.Response{
 				Status: 200,
 				Body:   getSimpleProductResponseFixture(),
 			})
 
-		// @TODO: Remove when serach is ready: This is just for the time where search is not calling the priceengine
-		pact.AddInteraction().
-			Given("TEMP: Priceegine product exists").
-			UponReceiving("A request to a priceengine for simple").
-			WithRequest(dsl.Request{
-				Method: "POST",
-				Path:   "/prices",
-			}).
-			WillRespondWith(dsl.Response{
-				Status: 200,
-				Body:   getPriceResponseFixture("searchperience-product-v2"),
-			})
-
 		if err := pact.Verify(func() error {
 
 			var productclient = product.ProductApiClient{}
 			productclient.SearchperienceClient.BaseURL = fmt.Sprintf("http://localhost:%d/", pact.Server.Port)
-			var productService = product.ProductService{Client: &productclient, Locale: "en_US", Channel: "ecommerce"}
+			var productService = product.ProductService{Client: &productclient, Locale: "en_EN", Channel: "onlinestore"}
 			productService.TempPriceEngineService.TempPriceEngineBaseURL = fmt.Sprintf("http://localhost:%d/", pact.Server.Port)
 
 			var product, err = productService.Get(context.Background(), "test")
@@ -61,10 +48,10 @@ func TestProductserviceCanGetSimpleProduct(t *testing.T) {
 				t.Fatalf("Product Title is expected to be %v got %v", simpleTitleFixture, product.BaseData().Title)
 			}
 			// @TODO: Remove when search is ready: This is just for the time where search is not calling the priceengine
-			//simpleProduct := product.(domain.SimpleProduct)
-			//if simpleProduct.ActivePrice.Default != 9.99 {
-			//	t.Fatalf("Product ActivePrice is expected to be %v got %v", 9.99, simpleProduct.ActivePrice.Default)
-			//}
+			simpleProduct := product.(domain.SimpleProduct)
+			if simpleProduct.ActivePrice.Default != 9.99 {
+				t.Fatalf("Product ActivePrice is expected to be %v got %v", 9.99, simpleProduct.ActivePrice.Default)
+			}
 			return nil
 		}); err != nil {
 			t.Fatal(err)
@@ -85,31 +72,18 @@ func TestProductserviceCanGetConfigurableProduct(t *testing.T) {
 			UponReceiving("A request to a configurable test product").
 			WithRequest(dsl.Request{
 				Method: "GET",
-				Path:   "/product/configurabletestiden_USecommerce",
+				Path:   "/product/ffad06e1af1b070534ea1b639f948e1b_configurable-en_EN-mainstore",
 			}).
 			WillRespondWith(dsl.Response{
 				Status: 200,
 				Body:   getConfigurableProductResponseFixture(),
 			})
 
-		// @TODO: Remove when serach is ready: This is just for the time where search is not calling the priceengine
-		pact.AddInteraction().
-			Given("TEMP: Priceengine product exists").
-			UponReceiving("A request to a priceengine for configurable").
-			WithRequest(dsl.Request{
-				Method: "POST",
-				Path:   "/prices",
-			}).
-			WillRespondWith(dsl.Response{
-				Status: 200,
-				Body:   getPriceResponseFixture("searchperience-product-v2"),
-			})
-
 		if err := pact.Verify(func() error {
 
 			var productclient = product.ProductApiClient{}
 			productclient.SearchperienceClient.BaseURL = fmt.Sprintf("http://localhost:%d/", pact.Server.Port)
-			var productService = product.ProductService{Client: &productclient, Locale: "en_US", Channel: "ecommerce"}
+			var productService = product.ProductService{Client: &productclient, Locale: "en_EN", Channel: "mainstore"}
 
 			productService.TempPriceEngineService.TempPriceEngineBaseURL = fmt.Sprintf("http://localhost:%d/", pact.Server.Port)
 

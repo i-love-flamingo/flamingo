e8f1a501366f09396f59c10e17eb9b5f1f03288f
Thorsten Essig <thorsten.essig@aoe.com>
2017-11-06 13:42:28 +0100

    Remove useless wrap for logrus logger

--- core/logrus/module.go
+++ core/logrus/module.go
@@ -7,15 +7,11 @@ import (
 	"os"
 )
 
-// Module for logrus logging
 type (
+	// Module for logrus logging
 	Module struct {
 		Area string `inject:"config:area"`
 		JSON bool   `inject:"config:logrus.json,optional"`
-	}
-
-	logrusLogger struct {
-		*logrus.Logger
 	}
 
 	logrusEntry struct {
@@ -24,34 +20,18 @@ type (
 )
 
 func (m *Module) Configure(injector *dingo.Injector) {
-	var l flamingo.Logger
+	var l *logrus.Logger
 	if m.JSON {
-		l = &logrusLogger{
-			Logger: &logrus.Logger{
-				Out:       os.Stderr,
-				Formatter: new(logrus.JSONFormatter),
-				Hooks:     make(logrus.LevelHooks),
-				Level:     logrus.InfoLevel,
-			},
+		l = &logrus.Logger{
+			Out:       os.Stderr,
+			Formatter: new(logrus.JSONFormatter),
+			Hooks:     make(logrus.LevelHooks),
+			Level:     logrus.InfoLevel,
 		}
 	} else {
-		l = &logrusLogger{
-			Logger: logrus.New(),
-		}
+		l = logrus.New()
 	}
-	injector.Bind((*flamingo.Logger)(nil)).ToInstance(l.WithField("area", m.Area))
-}
-
-func (l *logrusLogger) WithField(key string, value interface{}) flamingo.Logger {
-	return &logrusEntry{l.Logger.WithField(key, value)}
-}
-
-func (l *logrusLogger) WithFields(fields map[string]interface{}) flamingo.Logger {
-	return &logrusEntry{l.Logger.WithFields(fields)}
-}
-
-func (l *logrusLogger) WithError(err error) flamingo.Logger {
-	return &logrusEntry{l.Logger.WithError(err)}
+	injector.Bind((*flamingo.Logger)(nil)).ToInstance(&logrusEntry{l.WithField("area", m.Area)})
 }
 
 func (e *logrusEntry) WithField(key string, value interface{}) flamingo.Logger {

043a684820dc604021c161c6eefdff8e1465a158
Bastian Ike <bastian.ike@aoe.com>
2017-11-10 09:59:33 +0100

    framework: fix url-helper to not mismatch due to multiple repleacements

--- framework/web/utils.go
+++ framework/web/utils.go
@@ -8,7 +8,9 @@ import (
 func URLTitle(title string) string {
 	url := strings.ToLower(strings.Replace(strings.Replace(title, "/", "_", -1), " ", "-", -1))
 	url = strings.Replace(url, "-_", "-", -1)
-	url = strings.Replace(url, "--", "-", -1)
+	for strings.Contains(url, "--") {
+		url = strings.Replace(url, "--", "-", -1)
+	}
 
 	return url
 }

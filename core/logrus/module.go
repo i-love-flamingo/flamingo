c0077500db07642c603e624452ceafd518e88da1
Bastian Ike <bastian.ike@aoe.com>
2017-11-17 17:29:02 +0100

    logrus: filename+func

--- core/logrus/module.go
+++ core/logrus/module.go
@@ -1,10 +1,14 @@
 package logrus
 
 import (
+	"os"
+	"path"
+	"runtime"
+	"strings"
+
 	"github.com/sirupsen/logrus"
 	"go.aoe.com/flamingo/framework/dingo"
 	"go.aoe.com/flamingo/framework/flamingo"
-	"os"
 )
 
 type (
@@ -17,7 +21,31 @@ type (
 	LogrusEntry struct {
 		*logrus.Entry
 	}
+
+	ContextHook struct{}
 )
+
+func (hook ContextHook) Levels() []logrus.Level {
+	return logrus.AllLevels
+}
+
+func (hook ContextHook) Fire(entry *logrus.Entry) error {
+	pc := make([]uintptr, 3, 3)
+	cnt := runtime.Callers(6, pc)
+
+	for i := 0; i < cnt; i++ {
+		fu := runtime.FuncForPC(pc[i] - 1)
+		name := fu.Name()
+		if !strings.Contains(name, "github.com/sirupsen/logrus") {
+			file, line := fu.FileLine(pc[i] - 1)
+			entry.Data["file"] = path.Base(file)
+			entry.Data["func"] = path.Base(name)
+			entry.Data["line"] = line
+			break
+		}
+	}
+	return nil
+}
 
 func (m *Module) Configure(injector *dingo.Injector) {
 	var l *logrus.Logger
@@ -31,6 +59,7 @@ func (m *Module) Configure(injector *dingo.Injector) {
 	} else {
 		l = logrus.New()
 	}
+	l.Hooks.Add(ContextHook{})
 	injector.Bind((*flamingo.Logger)(nil)).ToInstance(&LogrusEntry{l.WithField("area", m.Area)})
 }
 

23159e2133053eb50cef630a25ee3a489ec5a329
Bastian Ike <bastian.ike@aoe.com>
2017-11-24 12:15:01 +0100

    pugtemplate: add `data` method

--- core/pugtemplate/module.go
+++ core/pugtemplate/module.go
@@ -58,6 +58,7 @@ func (m *Module) Configure(injector *dingo.Injector) {
 	injector.BindMulti((*template.Function)(nil)).To(templatefunctions.JsJSON{})
 	injector.BindMulti((*template.Function)(nil)).To(templatefunctions.URLFunc{})
 	injector.BindMulti((*template.ContextFunction)(nil)).To(templatefunctions.GetFunc{})
+	injector.BindMulti((*template.ContextFunction)(nil)).To(templatefunctions.DataFunc{})
 	injector.BindMulti((*template.Function)(nil)).To(templatefunctions.PriceFormatFunc{})
 	injector.BindMulti((*template.Function)(nil)).To(templatefunctions.StriptagsFunc{})
 
---
+++ core/pugtemplate/templatefunctions/data_func.go
@@ -0,0 +1,32 @@
+package templatefunctions
+
+import (
+	"go.aoe.com/flamingo/core/pugtemplate/pugjs"
+	"go.aoe.com/flamingo/framework/router"
+	"go.aoe.com/flamingo/framework/web"
+)
+
+type (
+	// DataFunc allows templates to access the router's `get` method
+	DataFunc struct {
+		Router *router.Router `inject:""`
+	}
+)
+
+// Name alias for use in template
+func (g DataFunc) Name() string {
+	return "data"
+}
+
+// Func as implementation of get method
+func (g *DataFunc) Func(ctx web.Context) interface{} {
+	return func(what string, params ...*pugjs.Map) interface{} {
+		var p = make(map[interface{}]interface{})
+		if len(params) == 1 {
+			for k, v := range params[0].Items {
+				p[k.String()] = v.String()
+			}
+		}
+		return g.Router.Get(what, ctx, p)
+	}
+}

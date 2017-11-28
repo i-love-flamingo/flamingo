2e485150d23ba03b4290eacc3a6a3ee96975dd84
Bastian Ike <bastian.ike@aoe.com>
2017-11-28 21:41:20 +0100

    pugjs: cleanup and fix tests

--- core/pugtemplate/pugjs/transform_js_test.go
+++ core/pugtemplate/pugjs/transform_js_test.go
@@ -3,177 +3,111 @@ package pugjs
 import (
 	"testing"
 
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
+	"github.com/stretchr/testify/assert"
 )
 
-var _ = Describe("JS Expression transpiling", func() {
+func TestJsExpr(t *testing.T) {
 	var s = newRenderState("/", true)
 
-	Describe("JsExpr modes", func() {
-		Context("With raw, wrap", func() {
-			It("Should treat code as escaped blocks of JavaScript", func() {
-				Expect(s.JsExpr(`var a = 1`, true, true)).To(Equal(`{{ $a := 1 -}}`))
-			})
-
-			It("Should fail with panic on invalid code", func() {
-				Expect(func() { s.JsExpr(`[1,2,`, false, true) }).To(Panic())
-			})
+	t.Run("JsExpr modes", func(t *testing.T) {
+		t.Run("With raw, wrap", func(t *testing.T) {
+			assert.Equal(t, `{{ $a := 1 -}}`, s.JsExpr(`var a = 1`, true, true))
+			assert.Panics(t, func() { s.JsExpr(`[1,2,`, false, true) })
 		})
 
-		Context("With raw, not wrap", func() {
-			It("Should treat code as escaped blocks of JavaScript", func() {
-				Expect(s.JsExpr(`var a = 1`, false, true)).To(Equal(`$a := 1`))
-			})
-
-			It("Should fail with panic on invalid code", func() {
-				Expect(func() { s.JsExpr(`[1,2,`, false, true) }).To(Panic())
-			})
+		t.Run("With raw, not wrap", func(t *testing.T) {
+			assert.Equal(t, `$a := 1`, s.JsExpr(`var a = 1`, false, true))
+			assert.Panics(t, func() { s.JsExpr(`[1,2,`, false, true) })
 		})
 
-		Context("With not raw, wrap", func() {
-			It("Should treat code as escaped blocks of JavaScript", func() {
-				Expect(s.JsExpr(`{"key": "value"}`, true, false)).To(Equal(`{{(__op__map "key" "value")}}`))
-			})
-
-			It("Should fail with panic on invalid code", func() {
-				Expect(func() { s.JsExpr(`[1,2,`, false, false) }).To(Panic())
-			})
+		t.Run("With not raw, wrap", func(t *testing.T) {
+			assert.Equal(t, `{{(__op__map "key" "value")}}`, s.JsExpr(`{"key": "value"}`, true, false))
+			assert.Panics(t, func() { s.JsExpr(`[1,2,`, false, false) })
 		})
 
-		Context("With not raw, not wrap", func() {
-			It("Should treat code as escaped blocks of JavaScript", func() {
-				Expect(s.JsExpr(`{"key": "value"}`, false, false)).To(Equal(`(__op__map "key" "value")`))
-			})
-
-			It("Should fail with panic on variable statements", func() {
-				Expect(func() { s.JsExpr(`var a = 1`, false, false) }).To(Panic())
-			})
+		t.Run("With not raw, not wrap", func(t *testing.T) {
+			assert.Equal(t, `(__op__map "key" "value")`, s.JsExpr(`{"key": "value"}`, false, false))
+			assert.Panics(t, func() { s.JsExpr(`var a = 1`, false, false) })
 		})
 	})
 
-	Describe("Function renderExpression", func() {
-		It("Should not fail for empty input", func() {
-			Expect(s.JsExpr(``, true, true)).To(Equal(""))
+	t.Run("Function renderExpression", func(t *testing.T) {
+		assert.Equal(t, "", s.JsExpr(``, true, true))
+
+		t.Run("Transpile Identifier", func(t *testing.T) {
+			assert.Equal(t, `{{$testknown | __pug__html}}`, s.JsExpr(`testknown`, true, true))
+			assert.Equal(t, `{{$testknown | __pug__html}}`, s.JsExpr(`testknown`, true, true))
+
+			s.rawmode = true
+			assert.Equal(t, `{{$testknown}}`, s.JsExpr(`testknown`, true, true))
+			s.rawmode = false
 		})
 
-		Context("Transpile Identifier", func() {
-			It("Should transpile it correctly if it is known", func() {
-				Expect(s.JsExpr(`testknown`, true, true)).To(Equal(`{{$testknown | __pug__html}}`))
-			})
-			It("Should transpile it correctly if it is not known", func() {
-				Expect(s.JsExpr(`testknown`, true, true)).To(Equal(`{{$testknown | __pug__html}}`))
-			})
-			It("Should make it raw if rawmode is on", func() {
-				s.rawmode = true
-				Expect(s.JsExpr(`testknown`, true, true)).To(Equal(`{{$testknown}}`))
-				s.rawmode = false
-			})
+		t.Run("Transpile String Literal", func(t *testing.T) {
+			assert.Equal(t, `{{(__str "foo" $a " $" (__op__add 1 2) )}}`, s.JsExpr(`"foo${a} \$${1+2}"`, true, false))
+
+			assert.Equal(t, `test`, s.JsExpr(`"test"`, true, false))
+			assert.Equal(t, `"test"`, s.JsExpr(`"test"`, false, false))
+			assert.Equal(t, `&lt;test&gt;`, s.JsExpr(`"<test>"`, true, false))
+			assert.Equal(t, `"<test>"`, s.JsExpr(`"<test>"`, false, false))
 		})
 
-		Context("Transpile String Literal", func() {
-			It("Should interpolate if necessary", func() {
-				Expect(s.JsExpr(`"foo${a} \$${1+2}"`, true, false)).To(Equal(`{{(__str "foo" $a " $" (__op__add 1 2) )}}`))
-			})
-
-			It("Should strip unnecessary template stuff for raw strings without interpolation", func() {
-				Expect(s.JsExpr(`"test"`, true, false)).To(Equal(`test`))
-				Expect(s.JsExpr(`"test"`, false, false)).To(Equal(`"test"`))
-				Expect(s.JsExpr(`"<test>"`, true, false)).To(Equal(`&lt;test&gt;`))
-				Expect(s.JsExpr(`"<test>"`, false, false)).To(Equal(`"<test>"`))
-			})
+		t.Run("Transpile Array Literal", func(t *testing.T) {
+			assert.Equal(t, `{{(__op__array 1 2 3)}}`, s.JsExpr(`[1, 2, 3]`, true, false))
+			assert.Equal(t, `(__op__array 1 2 3)`, s.JsExpr(`[1, 2, 3]`, false, false))
 		})
 
-		Context("Transpile Array Literal", func() {
-			It("Should map arrays to __op__array", func() {
-				Expect(s.JsExpr(`[1, 2, 3]`, true, false)).To(Equal(`{{(__op__array 1 2 3)}}`))
-				Expect(s.JsExpr(`[1, 2, 3]`, false, false)).To(Equal(`(__op__array 1 2 3)`))
-			})
+		t.Run("Transpile Boolean expression", func(t *testing.T) {
+			assert.Equal(t, `true`, s.JsExpr(`true`, false, false))
+			assert.Equal(t, `false`, s.JsExpr(`false`, false, false))
 		})
 
-		Context("Transpile Boolean expression", func() {
-			It("Should be true and false", func() {
-				Expect(s.JsExpr(`true`, false, false)).To(Equal(`true`))
-				Expect(s.JsExpr(`false`, false, false)).To(Equal(`false`))
-			})
+		t.Run("Transpile Map Literal", func(t *testing.T) {
+			assert.Equal(t, `{{(__op__map "key" 1 "key2" (__op__map "key1" (__op__array (__op__add 1 2) 3 4)))}}`, s.JsExpr(`{"key": 1, "key2": {"key1": [1+2, 3, 4]}}`, true, false))
+			assert.Equal(t, `(__op__map "key" 1 "key2" (__op__map "key1" (__op__array (__op__add 1 2) 3 4)))`, s.JsExpr(`{"key": 1, "key2": {"key1": [1+2, 3, 4]}}`, false, false))
 		})
 
-		Context("Transpile Map Literal", func() {
-			It("Should map objects to __op__map", func() {
-				Expect(s.JsExpr(`{"key": 1, "key2": {"key1": [1+2, 3, 4]}}`, true, false)).To(Equal(`{{(__op__map "key" 1 "key2" (__op__map "key1" (__op__array (__op__add 1 2) 3 4)))}}`))
-				Expect(s.JsExpr(`{"key": 1, "key2": {"key1": [1+2, 3, 4]}}`, false, false)).To(Equal(`(__op__map "key" 1 "key2" (__op__map "key1" (__op__array (__op__add 1 2) 3 4)))`))
-			})
+		t.Run("Transpile Null Literal", func(t *testing.T) {
+			assert.Equal(t, `{{null}}`, s.JsExpr(`null`, true, false))
+			assert.Equal(t, ``, s.JsExpr(`null`, false, false))
 		})
 
-		Context("Transpile Null Literal", func() {
-			It("Should be null if wrapped", func() {
-				Expect(s.JsExpr(`null`, true, false)).To(Equal(`{{null}}`))
-			})
-			It("Should be empty if not wrapped", func() {
-				Expect(s.JsExpr(`null`, false, false)).To(Equal(``))
-			})
+		t.Run("Transpile Dot Expression", func(t *testing.T) {
+			assert.Equal(t, `$a.b`, s.JsExpr(`a.b`, false, false))
+
+			s.rawmode = true
+			assert.Equal(t, `{{$a.b}}`, s.JsExpr(`a.b`, true, false))
+			s.rawmode = false
 		})
 
-		Context("Transpile Dot Expression", func() {
-			It("Should use dot-notation", func() {
-				Expect(s.JsExpr(`a.b`, false, false)).To(Equal(`$a.b`))
-			})
-			It("Should be raw and escaped if rawmode and wrap is set", func() {
-				s.rawmode = true
-				Expect(s.JsExpr(`a.b`, true, false)).To(Equal(`{{$a.b}}`))
-				s.rawmode = false
-			})
+		t.Run("Transpile Conditional Expression", func(t *testing.T) {
+			//assert.Equal(t, (s.JsExpr(`a ? b : c`, false, false)).To(Equal(`{{if $a}}{{$b}}{{else}}{{$c}}{{end}}`))
+			//assert.Equal(t, (s.JsExpr(`a ? b : null`, false, false)).To(Equal(`{{if $a}}{{$b}}{{end}}`))
 		})
 
-		Context("Transpile Conditional Expression", func() {
-			It("Should become and if-else statement", func() {
-				//Expect(s.JsExpr(`a ? b : c`, false, false)).To(Equal(`{{if $a}}{{$b}}{{else}}{{$c}}{{end}}`))
-			})
-			It("Should eliminate null else branches", func() {
-				//Expect(s.JsExpr(`a ? b : null`, false, false)).To(Equal(`{{if $a}}{{$b}}{{end}}`))
-			})
+		t.Run("Transpile Binary Expressions", func(t *testing.T) {
+			assert.Equal(t, `{{(__op__b_and $a $b)}}`, s.JsExpr(`a & b`, true, false))
 		})
 
-		Context("Transpile Binary Expressions", func() {
-			It("Should handle &-operator", func() {
-				Expect(s.JsExpr(`a & b`, true, false)).To(Equal(`{{(__op__b_and $a $b)}}`))
-			})
+		t.Run("Transpile Call Expressions", func(t *testing.T) {
+			s.funcs = FuncMap{"foo": func(int, int) {}}
+			assert.Equal(t, `{{(foo (__op__add 1 2)) | __pug__html}}`, s.JsExpr(`foo(1+2)`, true, false))
 		})
 
-		Context("Transpile Call Expressions", func() {
-			It("Should transform js-call-syntax to go template call syntax", func() {
-				s.funcs = FuncMap{"foo": func(int, int) {}}
-				Expect(s.JsExpr(`foo(1+2)`, true, false)).To(Equal(`{{(foo (__op__add 1 2)) | __pug__html}}`))
-			})
+		t.Run("Transpile Assign Expressions", func(t *testing.T) {
+			assert.Equal(t, `{{ $a := 1 -}}`, s.JsExpr(`a = 1`, true, false))
 		})
 
-		Context("Transpile Assign Expressions", func() {
-			It("Should assign expressions to variables", func() {
-				Expect(s.JsExpr(`a = 1`, true, false)).To(Equal(`{{ $a := 1 -}}`))
-			})
+		t.Run("Transpile Sequence Expression", func(t *testing.T) {
+			assert.Equal(t, `(__op__array 1 2 3)`, s.JsExpr(`1,2,3`, true, false))
 		})
 
-		Context("Transpile Sequence Expression", func() {
-			It("Should use __op__array and not wrap", func() {
-				Expect(s.JsExpr(`1,2,3`, true, false)).To(Equal(`(__op__array 1 2 3)`))
-			})
-		})
-
-		Context("Transpile Bracket Expression", func() {
-			It("Should use the index function to access the specified element", func() {
-				Expect(s.JsExpr(`a[0][b[1]]`, true, false)).To(Equal(`{{(index (index $a 0) (index $b 1)) | __pug__html}}`))
-			})
+		t.Run("Transpile Bracket Expression", func(t *testing.T) {
+			assert.Equal(t, `{{(__pug__index (__pug__index $a 0) (__pug__index $b 1)) | __pug__html}}`, s.JsExpr(`a[0][b[1]]`, true, false))
 		})
 	})
 
-	Describe("Known Bugs", func() {
-		It("Should render brand.heroImage.url to $brand.heroImage.url", func() {
-			Expect(s.JsExpr("`background-image:url(${brand.heroImage.url})`", false, false)).To(Equal(`(__str "background-image:url(" $brand.heroImage.url ")")`))
-		})
+	t.Run("Known Bugs", func(t *testing.T) {
+		assert.Equal(t, `(__str "background-image:url(" $brand.heroImage.url ")")`, s.JsExpr("`background-image:url(${brand.heroImage.url})`", false, false))
 	})
-})
-
-func TestJsExpr(t *testing.T) {
-	RegisterFailHandler(Fail)
-	RunSpecs(t, "JsExpr Suite")
 }

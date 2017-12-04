0acb0a32d3f0d75ee943ceac840284ab30bb1fba
Daniel Pötzinger <daniel.poetzinger@aoe.com>
2017-12-04 23:27:48 +0100

    fix datetime parsing

--- core/locale/application/dateTimeService.go
+++ core/locale/application/dateTimeService.go
@@ -10,28 +10,32 @@ import (
 
 type (
 	DateTimeService struct {
-		DateFormat     string          `inject:"config:locale.dateTime.dateFormat"`
-		TimeFormat     string          `inject:"config:locale.dateTime.timeFormat"`
-		DateTimeFormat string          `inject:"config:locale.dateTime.dateTimeFormat"`
-		Location       string          `inject:"config:locale.dateTime.location"`
+		DateFormat     string          `inject:"config:locale.date.dateFormat"`
+		TimeFormat     string          `inject:"config:locale.date.timeFormat"`
+		DateTimeFormat string          `inject:"config:locale.date.dateTimeFormat"`
+		Location       string          `inject:"config:locale.date.location"`
 		Logger         flamingo.Logger `inject:""`
 	}
 )
 
 //GetDateTimeFromString Need string in format ISO: "2017-11-25T06:30:00Z"
-func (dts *DateTimeService) GetDateTimeFromIsoString(dateTimeString string) (*domain.DateTime, error) {
-	//"scheduledDateTime": "2017-11-25T06:30:00Z",
+func (dts *DateTimeService) GetDateTimeFromIsoString(dateTimeString string) (*domain.DateTimeFormatter, error) {
 	timeResult, e := time.Parse(time.RFC3339, dateTimeString) //"2006-01-02T15:04:05Z"
 	if e != nil {
 		return nil, errors.Errorf("could not parse date in defined format: %v / Error: %v", dateTimeString, e)
 	}
-	dateTime := domain.DateTime{
+	loc, e := time.LoadLocation(dts.Location)
+	if e != nil {
+		if dts.Logger != nil {
+			dts.Logger.Errorf("dateTime Parsing error - could not load location %v", dts.Location)
+		}
+	}
+
+	dateTime := domain.DateTimeFormatter{
 		DateFormat:     dts.DateFormat,
 		TimeFormat:     dts.TimeFormat,
 		DateTimeFormat: dts.DateTimeFormat,
-		Location:       dts.Location,
-		Logger:         dts.Logger,
 	}
-	dateTime.SetDateTime(timeResult)
+	dateTime.SetDateTime(timeResult, timeResult.In(loc))
 	return &dateTime, nil
 }
--- core/locale/domain/dateTime.go
+++ core/locale/domain/dateTime.go
@@ -2,66 +2,53 @@ package domain
 
 import (
 	"time"
-
-	"go.aoe.com/flamingo/framework/flamingo"
 )
 
 type (
-	DateTime struct {
+	DateTimeFormatter struct {
 		DateFormat     string
 		TimeFormat     string
 		DateTimeFormat string
-		Location       string
-		Logger         flamingo.Logger
-		dateTime       time.Time
+
+		localDateTime time.Time
+		dateTime      time.Time
 	}
 )
 
 //SetDateTime Setter for private member
-func (dt *DateTime) SetDateTime(time time.Time) {
+func (dt *DateTimeFormatter) SetDateTime(time time.Time, localtime time.Time) {
 	dt.dateTime = time
+	dt.localDateTime = localtime
 }
 
-func (dts *DateTime) Format(format string) string {
+func (dts *DateTimeFormatter) Format(format string) string {
 	return dts.dateTime.Format(format)
 }
 
-func (dts *DateTime) FormatLocale(format string) string {
-	return dts.getLocalTime().Format(format)
+func (dts *DateTimeFormatter) FormatLocale(format string) string {
+	return dts.localDateTime.Format(format)
 }
 
-func (dts *DateTime) FormatDate() string {
-	dts.Logger.Errorf("ääääääääää %v", dts.dateTime)
+func (dts *DateTimeFormatter) FormatDate() string {
 	return dts.dateTime.Format(dts.DateFormat)
 }
 
-func (dts *DateTime) FormatTime(dateTimeString string) string {
+func (dts *DateTimeFormatter) FormatTime() string {
 	return dts.dateTime.Format(dts.TimeFormat)
 }
 
-func (dts *DateTime) FormatDateTime(dateTimeString string) string {
+func (dts *DateTimeFormatter) FormatDateTime() string {
 	return dts.dateTime.Format(dts.DateTimeFormat)
 }
 
-func (dts *DateTime) FormatToLocalDate() string {
-	return dts.getLocalTime().Format(dts.DateFormat)
+func (dts *DateTimeFormatter) FormatToLocalDate() string {
+	return dts.localDateTime.Format(dts.DateFormat)
 }
 
-func (dts *DateTime) FormatToLocalTime(dateTimeString string) string {
-	return dts.getLocalTime().Format(dts.TimeFormat)
+func (dts *DateTimeFormatter) FormatToLocalTime() string {
+	return dts.localDateTime.Format(dts.TimeFormat)
 }
 
-func (dts *DateTime) FormatToLocalDateTime(dateTimeString string) string {
-	return dts.getLocalTime().Format(dts.DateTimeFormat)
-}
-
-func (dts *DateTime) getLocalTime() time.Time {
-	loc, e := time.LoadLocation(dts.Location)
-	if e != nil {
-		if dts.Logger != nil {
-			dts.Logger.Errorf("dateTime Parsing error - could not load location %v", dts.Location)
-		}
-		return dts.dateTime
-	}
-	return dts.dateTime.In(loc)
+func (dts *DateTimeFormatter) FormatToLocalDateTime() string {
+	return dts.localDateTime.Format(dts.DateTimeFormat)
 }

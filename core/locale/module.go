5b3b2625a6918af67424721de403ca9752c9e1db
Bastian Ike <bastian.ike@aoe.com>
2017-12-04 23:05:58 +0100

    locale: fix typo

--- core/locale/module.go
+++ core/locale/module.go
@@ -32,7 +32,7 @@ func (m *Module) DefaultConfig() config.Map {
 			"formatZero": "%s -,-",
 			"format":     "%s %v",
 		},
-		"locale.dateFomat": config.Map{
+		"locale.dateFormat": config.Map{
 			"dateFormat":     "02 Jan 2006",
 			"timeFormat":     "15:04:05",
 			"dateTimeFormat": "02 Jan 2006 15:04:05",

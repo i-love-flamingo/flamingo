c7670732f7f9e5331f16a7c38f95f6d55d29ec76
Daniel Pötzinger <daniel.poetzinger@aoe.com>
2018-01-16 14:28:49 +0100

    make debuglevel configurable / add source field to logoutput

--- core/logrus/module.go
+++ core/logrus/module.go
@@ -8,6 +8,8 @@ import (
 
 	"sync"
 
+	"fmt"
+
 	"github.com/sirupsen/logrus"
 	"go.aoe.com/flamingo/framework/dingo"
 	"go.aoe.com/flamingo/framework/flamingo"
@@ -16,8 +18,9 @@ import (
 type (
 	// Module for logrus logging
 	Module struct {
-		Area string `inject:"config:area"`
-		JSON bool   `inject:"config:logrus.json,optional"`
+		Area     string `inject:"config:area"`
+		JSON     bool   `inject:"config:logrus.json,optional"`
+		LogLevel string `inject:"config:logrus.loglevel,optional"`
 	}
 
 	LogrusEntry struct {
@@ -53,13 +56,22 @@ func (hook ContextHook) Fire(entry *logrus.Entry) error {
 		name := fu.Name()
 		if !strings.Contains(name, "github.com/sirupsen/logrus") {
 			file, line := fu.FileLine(pc[i] - 1)
-			entry.Data["file"] = path.Base(file)
-			entry.Data["func"] = path.Base(name)
-			entry.Data["line"] = line
+			entry.Data["source"] = fmt.Sprintf("File: %v Func: %v  Line: %v", lastPathAndFileName(file), path.Base(name), line)
+			entry.Data["fileName"] = path.Base(file)
 			break
 		}
 	}
 	return nil
+}
+
+//lastPathAndFileName returns the filename and the last two folders of the Path
+func lastPathAndFileName(completePath string) string {
+	path, fileName := path.Split(completePath)
+	dirNames := strings.Split(strings.Trim(path, "/"), "/")
+	if len(dirNames) > 2 {
+		return dirNames[len(dirNames)-2] + "/" + dirNames[len(dirNames)-1] + "/" + fileName
+	}
+	return fileName
 }
 
 func (m *Module) Configure(injector *dingo.Injector) {
@@ -69,10 +81,15 @@ func (m *Module) Configure(injector *dingo.Injector) {
 			Out:       os.Stderr,
 			Formatter: new(logrus.JSONFormatter),
 			Hooks:     make(logrus.LevelHooks),
-			Level:     logrus.InfoLevel,
 		}
 	} else {
 		l = logrus.New()
+	}
+	l.Level = logrus.ErrorLevel
+	if m.LogLevel == "Info" {
+		l.Level = logrus.InfoLevel
+	} else if m.LogLevel == "Debug" {
+		l.Level = logrus.DebugLevel
 	}
 	l.Hooks.Add(ContextHook{area: m.Area})
 	injector.Bind((*flamingo.Logger)(nil)).ToInstance(&LogrusLogger{l})

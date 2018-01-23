f6c014c7e730f718b2649b2ee88732085da46a20
Mathias Schopmans <mathias.schopmans@aoe.com>
2018-01-23 15:54:22 +0100

    Remove comments in startsWith module

--- core/pugtemplate/templatefunctions/startswith_func.go
+++ core/pugtemplate/templatefunctions/startswith_func.go
@@ -5,16 +5,13 @@ import (
 )
 
 type (
-	// startsWithFunc returns boolean
 	StartsWithFunc struct{}
 )
 
-// Name alias for use in template
 func (s StartsWithFunc) Name() string {
 	return "startsWith"
 }
 
-// Func as implementation of url method
 func (s *StartsWithFunc) Func() interface{} {
 	return func(haystack string, needle string) bool {
 		haystack = strings.ToLower(haystack)

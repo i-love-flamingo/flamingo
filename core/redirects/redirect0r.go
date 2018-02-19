8d41fa59c65764cfd4c09f07e3b9520e16d7b200
Joachim Adomeit <joachim.adomeit@aoe.com>
2018-02-19 13:14:32 +0100

    158705: [FEATURE] core/redirects kickstart

---
+++ core/redirects/redirect0r.go
@@ -0,0 +1,49 @@
+package redirects
+
+import (
+	"fmt"
+	"net/http"
+
+	"go.aoe.com/flamingo/core/redirects/infrastructure"
+	"go.aoe.com/flamingo/framework/flamingo"
+	"go.aoe.com/flamingo/framework/router"
+	"go.aoe.com/flamingo/framework/web"
+	"go.aoe.com/flamingo/framework/web/responder"
+
+	"go.aoe.com/flamingo/core/cache"
+)
+
+type (
+	redirect0r struct {
+		responder.RedirectAware                `inject:""`
+		cache.Backend                          `inject:""`
+		Logger flamingo.Logger                 `inject:""`
+	}
+)
+
+var redirectData []infrastructure.CsvContent
+
+func init() {
+	redirectData = infrastructure.GetRedirectData()
+}
+
+func (r *redirect0r) Filter(ctx web.Context, w http.ResponseWriter, chain *router.FilterChain) {
+	for i := range redirectData {
+		if redirectData[i].OriginalPath == ctx.Request().URL.RawPath {
+			r.redirectFlow(ctx.Request(), w, redirectData[i])
+		}
+	}
+}
+
+func (r *redirect0r) redirectFlow(req *http.Request, w http.ResponseWriter, redirectInfo infrastructure.CsvContent) {
+	switch code := redirectInfo.HttpStatusCode; code {
+	case http.StatusGone:
+		r.Redirect(fmt.Sprintf("/%d", http.StatusNotFound), nil)
+	case http.StatusMovedPermanently:
+		r.RedirectPermanent(fmt.Sprintf("/%s", redirectInfo.RedirectTarget), nil)
+	case http.StatusFound:
+		r.Redirect(fmt.Sprintf("/%s", redirectInfo.RedirectTarget), nil)
+	}
+
+	r.Redirect(fmt.Sprintf("/%d", http.StatusNotFound), nil)
+}

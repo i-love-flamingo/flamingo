62825f2f56335c91a27e43830018052078ac6fb3
Joachim Adomeit <joachim.adomeit@aoe.com>
2018-02-19 17:45:25 +0100

    158705: [FEATURE] core/redirects kickstart

--- core/redirects/redirect0r.go
+++ core/redirects/redirect0r.go
@@ -10,47 +10,46 @@ import (
 	"go.aoe.com/flamingo/framework/router"
 	"go.aoe.com/flamingo/framework/web"
 	"go.aoe.com/flamingo/framework/web/responder"
-
-	"go.aoe.com/flamingo/core/cache"
 )
 
 type (
 	redirect0r struct {
-		responder.RedirectAware      `inject:""`
-		responder.ErrorAware         `inject:""`
-		cache.Backend                `inject:""`
-		Logger       flamingo.Logger `inject:""`
-		redirectData []infrastructure.CsvContent
+		responder.RedirectAware `inject:""`
+		responder.ErrorAware    `inject:""`
+		Logger flamingo.Logger  `inject:""`
 	}
 )
 
-var redirectData []infrastructure.CsvContent
+var redirectDataMap map[string]infrastructure.CsvContent
 
 func init() {
-	redirectData = infrastructure.GetRedirectData()
+	redirectData := infrastructure.GetRedirectData()
+
+	redirectDataMap = make(map[string]infrastructure.CsvContent)
+
+	for i := range redirectData {
+		redirectDataMap[redirectData[i].OriginalPath] = redirectData[i]
+	}
 }
 
 func (r *redirect0r) Filter(ctx web.Context, w http.ResponseWriter, chain *router.FilterChain) web.Response {
 	contextPath := ctx.Request().RequestURI
 
-	for i := range redirectData {
-		originalPath := redirectData[i].OriginalPath
+	if currentRedirect, ok := redirectDataMap[contextPath]; ok {
+		httpCode := currentRedirect.HttpStatusCode
+		originalPath := currentRedirect.OriginalPath
+		redirectTarget := currentRedirect.RedirectTarget
 
-		if originalPath == contextPath {
-			redirectTarget := redirectData[i].RedirectTarget
-			httpCode := redirectData[i].HttpStatusCode
+		r.Logger.Debugf("Redirecting from %s to %s by %d", originalPath, redirectTarget, httpCode)
 
-			r.Logger.Debugf("Redirecting from %s to %s by %d", originalPath, redirectTarget, httpCode)
-
-			switch code := httpCode; code {
-			case http.StatusMovedPermanently:
-				return r.RedirectPermanentURL(fmt.Sprintf("%s", redirectTarget))
-			case http.StatusFound:
-				return r.RedirectURL(fmt.Sprintf("%s", redirectTarget))
-			}
-
-			return r.ErrorAware.ErrorNotFound(ctx, errors.New("page not found"))
+		switch code := httpCode; code {
+		case http.StatusMovedPermanently:
+			return r.RedirectPermanentURL(fmt.Sprintf("%s", redirectTarget))
+		case http.StatusFound:
+			return r.RedirectURL(fmt.Sprintf("%s", redirectTarget))
 		}
+
+		return r.ErrorAware.ErrorNotFound(ctx, errors.New("page not found"))
 	}
 
 	return chain.Next(ctx, w)

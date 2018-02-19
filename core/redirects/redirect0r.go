4b67b9c04dcd7c77e98ecb2933ae462ae3c6f868
Joachim Adomeit <joachim.adomeit@aoe.com>
2018-02-19 16:26:41 +0100

    158705: [FEATURE] core/redirects kickstart

--- core/redirects/redirect0r.go
+++ core/redirects/redirect0r.go
@@ -1,6 +1,7 @@
 package redirects
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 
@@ -15,9 +16,11 @@ import (
 
 type (
 	redirect0r struct {
-		responder.RedirectAware                `inject:""`
-		cache.Backend                          `inject:""`
-		Logger flamingo.Logger                 `inject:""`
+		responder.RedirectAware      `inject:""`
+		responder.ErrorAware         `inject:""`
+		cache.Backend                `inject:""`
+		Logger       flamingo.Logger `inject:""`
+		redirectData []infrastructure.CsvContent
 	}
 )
 
@@ -28,24 +31,27 @@ func init() {
 }
 
 func (r *redirect0r) Filter(ctx web.Context, w http.ResponseWriter, chain *router.FilterChain) web.Response {
+	contextPath := ctx.Request().RequestURI
+
 	for i := range redirectData {
-		if redirectData[i].OriginalPath == ctx.Request().URL.RawPath {
-			r.redirectFlow(ctx.Request(), w, redirectData[i])
+		originalPath := redirectData[i].OriginalPath
+
+		if originalPath == contextPath {
+			redirectTarget := redirectData[i].RedirectTarget
+			httpCode := redirectData[i].HttpStatusCode
+
+			r.Logger.Debugf("Redirecting from %s to %s by %d", originalPath, redirectTarget, httpCode)
+
+			switch code := httpCode; code {
+			case http.StatusMovedPermanently:
+				return r.RedirectPermanentURL(fmt.Sprintf("%s", redirectTarget))
+			case http.StatusFound:
+				return r.RedirectURL(fmt.Sprintf("%s", redirectTarget))
+			}
+
+			return r.ErrorAware.ErrorNotFound(ctx, errors.New("page not found"))
 		}
 	}
 
 	return chain.Next(ctx, w)
-}
-
-func (r *redirect0r) redirectFlow(req *http.Request, w http.ResponseWriter, redirectInfo infrastructure.CsvContent) {
-	switch code := redirectInfo.HttpStatusCode; code {
-	case http.StatusGone:
-		r.Redirect(fmt.Sprintf("/%d", http.StatusNotFound), nil)
-	case http.StatusMovedPermanently:
-		r.RedirectPermanent(fmt.Sprintf("/%s", redirectInfo.RedirectTarget), nil)
-	case http.StatusFound:
-		r.Redirect(fmt.Sprintf("/%s", redirectInfo.RedirectTarget), nil)
-	}
-
-	r.Redirect(fmt.Sprintf("/%d", http.StatusNotFound), nil)
 }

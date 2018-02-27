9abd90eff2f954d1834b96e3812368c33db11074
Thomas Neumann <thomas.neumann@aoe.com>
2018-02-27 11:16:48 +0100

    159261: Use configurable session lifetime

--- core/session/module.go
+++ core/session/module.go
@@ -12,34 +12,39 @@ import (
 
 // Module for session management
 type Module struct {
-	Backend          string `inject:"config:session.backend"`
-	Secret           string `inject:"config:session.secret"`
-	RedisHost        string `inject:"config:session.redis.host"`
-	FileName         string `inject:"config:session.file"`
-	StoreLength      int    `inject:"config:session.store.length"`
-	RedisStoreLength int    `inject:"config:session.redis.store.length"`
-	MaxAge           int    `inject:"config:session.redis.max.age"`
+	// session config is optional to allow usage of the DefaultConfig
+	Backend  string `inject:"config:session.backend,optional"`
+	Secret   string `inject:"config:session.secret,optional"`
+	FileName string `inject:"config:session.file,optional"`
+	// float64 is used due to the injection as config from json - int is not possible on this
+	StoreLength      float64 `inject:"config:session.store.length,optional"`
+	MaxAge           float64 `inject:"config:session.max.age,optional"`
+	RedisHost        string  `inject:"config:session.redis.host,optional"`
+	RedisStoreLength float64 `inject:"config:session.redis.store.length,optional"`
 }
 
 // Configure DI
 func (m *Module) Configure(injector *dingo.Injector) {
 	switch m.Backend {
 	case "redis":
-		sessionStore, err := redistore.NewRediStore(m.RedisStoreLength, "tcp", m.RedisHost, "", []byte(m.Secret))
+		sessionStore, err := redistore.NewRediStore(int(m.RedisStoreLength), "tcp", m.RedisHost, "", []byte(m.Secret))
 		if err != nil {
 			panic(err)
 		}
-		sessionStore.SetMaxLength(m.StoreLength)
-		sessionStore.DefaultMaxAge = m.MaxAge
+		sessionStore.SetMaxLength(int(m.StoreLength))
+		sessionStore.SetMaxAge(int(m.MaxAge))
+		sessionStore.DefaultMaxAge = int(m.MaxAge)
 		injector.Bind((*sessions.Store)(nil)).ToInstance(sessionStore)
 	case "file":
 		os.Mkdir(m.FileName, os.ModePerm)
 		sessionStore := sessions.NewFilesystemStore(m.FileName, []byte(m.Secret))
-		sessionStore.MaxLength(m.StoreLength)
+		sessionStore.MaxLength(int(m.StoreLength))
+		sessionStore.MaxAge(int(m.MaxAge))
 		injector.Bind((*sessions.Store)(nil)).ToInstance(sessionStore)
 	default: //memory
 		sessionStore := memorystore.NewMemoryStore([]byte(m.Secret))
-		sessionStore.MaxLength(m.StoreLength)
+		sessionStore.MaxLength(int(m.StoreLength))
+		sessionStore.MaxAge(int(m.MaxAge))
 		injector.Bind((*sessions.Store)(nil)).ToInstance(sessionStore)
 	}
 }
@@ -49,10 +54,10 @@ func (m *Module) DefaultConfig() config.Map {
 	return config.Map{
 		"session.backend":            "memory",
 		"session.secret":             "flamingosecret",
-		"session.redis.host":         "redis",
 		"session.file":               "/sessions",
 		"session.store.length":       1024 * 1024,
+		"session.max.age":            60 * 60 * 24 * 30,
+		"session.redis.host":         "redis",
 		"session.redis.store.length": 10,
-		"session.redis.max.age":      60 * 60 * 24 * 30,
 	}
 }

03e5bf0b5184d34884628413ce3a70dcdf1a4c5f
Thomas Neumann <thomas.neumann@aoe.com>
2018-02-27 14:20:47 +0100

    159261: Refactor session config for redis

--- core/session/module.go
+++ core/session/module.go
@@ -17,23 +17,22 @@ type Module struct {
 	Secret   string `inject:"config:session.secret,optional"`
 	FileName string `inject:"config:session.file,optional"`
 	// float64 is used due to the injection as config from json - int is not possible on this
-	StoreLength      float64 `inject:"config:session.store.length,optional"`
-	MaxAge           float64 `inject:"config:session.max.age,optional"`
-	RedisHost        string  `inject:"config:session.redis.host,optional"`
-	RedisStoreLength float64 `inject:"config:session.redis.store.length,optional"`
+	StoreLength          float64 `inject:"config:session.store.length,optional"`
+	MaxAge               float64 `inject:"config:session.max.age,optional"`
+	RedisHost            string  `inject:"config:session.redis.host,optional"`
+	RedisIdleConnections float64 `inject:"config:session.redis.idle.connections,optional"`
 }
 
 // Configure DI
 func (m *Module) Configure(injector *dingo.Injector) {
 	switch m.Backend {
 	case "redis":
-		sessionStore, err := redistore.NewRediStore(int(m.RedisStoreLength), "tcp", m.RedisHost, "", []byte(m.Secret))
+		sessionStore, err := redistore.NewRediStore(int(m.RedisIdleConnections), "tcp", m.RedisHost, "", []byte(m.Secret))
 		if err != nil {
 			panic(err)
 		}
-		sessionStore.SetMaxLength(int(m.StoreLength))
 		sessionStore.SetMaxAge(int(m.MaxAge))
-		sessionStore.DefaultMaxAge = int(m.MaxAge)
+		sessionStore.SetMaxLength(int(m.StoreLength))
 		injector.Bind((*sessions.Store)(nil)).ToInstance(sessionStore)
 	case "file":
 		os.Mkdir(m.FileName, os.ModePerm)
@@ -52,12 +51,12 @@ func (m *Module) Configure(injector *dingo.Injector) {
 // DefaultConfig for this module
 func (m *Module) DefaultConfig() config.Map {
 	return config.Map{
-		"session.backend":            "memory",
-		"session.secret":             "flamingosecret",
-		"session.file":               "/sessions",
-		"session.store.length":       1024 * 1024,
-		"session.max.age":            60 * 60 * 24 * 30,
-		"session.redis.host":         "redis",
-		"session.redis.store.length": 10,
+		"session.backend":                "memory",
+		"session.secret":                 "flamingosecret",
+		"session.file":                   "/sessions",
+		"session.store.length":           1024 * 1024,
+		"session.max.age":                60 * 60 * 24 * 30,
+		"session.redis.host":             "redis",
+		"session.redis.idle.connections": 10,
 	}
 }

fa2f184ae607b883f924b5099a9f007a9d9e8a1f
Daniel Pötzinger <daniel.poetzinger@aoe.com>
2018-03-17 22:57:42 +0100

    Allow redirects to be used in front_router

--- core/redirects/module.go
+++ core/redirects/module.go
@@ -1,17 +1,28 @@
 package redirects
 
 import (
+	"go.aoe.com/flamingo/core/prefixrouter"
 	"go.aoe.com/flamingo/framework/dingo"
 	"go.aoe.com/flamingo/framework/router"
 )
 
 // Module for core/redirects
 type (
-	Module struct{}
+	Module struct {
+		UseInRouter       bool `inject:"config:redirects.useInRouter,optional"`
+		UseInPrefixRouter bool `inject:"config:redirects.useInPrefixRouter,optional"`
+	}
 	LogFilter struct{}
 )
 
 // Configure DI
 func (m *Module) Configure(injector *dingo.Injector) {
-	injector.BindMulti((*router.Filter)(nil)).To(redirector{})
+	if m.UseInRouter {
+		injector.BindMulti((*router.Filter)(nil)).To(redirector{})
+	}
+
+	if m.UseInPrefixRouter {
+		injector.BindMulti((*prefixrouter.OptionalHandler)(nil)).AnnotatedWith("primaryHandlers").To(redirector{})
+	}
+
 }

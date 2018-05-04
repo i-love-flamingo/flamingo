9304e3f608b815af98cb4d1009ad4d702c086c14
Bastian Ike <bastian.ike@aoe.com>
2018-05-04 15:17:01 +0200

    cache: experimental file backend demo

--- core/cache/fileBackend.go
+++ core/cache/fileBackend.go
@@ -1,15 +1,51 @@
-package cache
+import (
+	"bytes"
+	"encoding/gob"
+	"io/ioutil"
+	"os"
+	"regexp"
+)
 
 type (
-	NullBackend struct{}
+	FileBackend struct{}
 )
 
 var (
-	_ Backend = &NullBackend{}
+	_ Backend = &FileBackend{}
+
+	escape = regexp.MustCompile(`[^a-zA-Z0-9.]`)
 )
 
-func (*NullBackend) Get(key string) (entry *Entry, found bool) { return nil, false }
-func (*NullBackend) Set(key string, entry *Entry)              {}
-func (*NullBackend) Purge(key string)                          {}
-func (*NullBackend) PurgeTags(tags []string)                   {}
-func (*NullBackend) Flush()                                    {}
+func (*FileBackend) Get(key string) (entry *Entry, found bool) {
+	key = escape.ReplaceAllString(key, ".")
+
+	b, err := ioutil.ReadFile("/tmp/cache/" + key)
+	if err != nil {
+		return nil, false
+	}
+
+	bb := bytes.NewBuffer(b)
+	d := gob.NewDecoder(bb)
+	entry = new(Entry)
+	d.Decode(&entry)
+	return entry, true
+}
+
+func (*FileBackend) Set(key string, entry *Entry) {
+	key = escape.ReplaceAllString(key, ".")
+
+	gob.Register(entry)
+
+	b := new(bytes.Buffer)
+	gob.NewEncoder(b).Encode(entry)
+
+	ioutil.WriteFile("/tmp/cache/"+key, b.Bytes(), os.ModePerm)
+}
+
+func (*FileBackend) Purge(key string) {
+	key = escape.ReplaceAllString(key, ".")
+	os.Remove("/tmp/cache/" + key)
+}
+
+func (*FileBackend) PurgeTags(tags []string) {}
+func (*FileBackend) Flush()                  {}

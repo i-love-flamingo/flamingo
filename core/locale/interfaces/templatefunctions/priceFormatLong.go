416186b3170178bbcdf8970065ad6a31a0479087
Thomas Layh <thomas.layh@aoe.com>
2018-05-14 16:56:32 +0200

    [Task] #167285 - Price format changes - adding function priceFormatLong

---
+++ core/locale/interfaces/templatefunctions/priceFormatLong.go
@@ -0,0 +1,35 @@
+package templatefunctions
+
+import (
+	"fmt"
+	"go.aoe.com/flamingo/core/locale/application"
+	"go.aoe.com/flamingo/framework/config"
+)
+
+type (
+	// PriceFormatFunc for formatting prices
+	PriceFormatLongFunc struct {
+		Config             config.Map                              `inject:"config:locale.accounting"`
+		TranslationService application.TranslationServiceInterface `inject:""`
+		PriceFormat        *PriceFormatFunc                        `inject:""`
+	}
+)
+
+// Name alias for use in template
+func (pff PriceFormatLongFunc) Name() string {
+	return "priceFormatLong"
+}
+
+// Func as implementation of debug method
+func (pff PriceFormatLongFunc) Func() interface{} {
+	return func(value interface{}, currency string, currencyLabel string) string {
+		priceFunc := pff.PriceFormat.Func().(func(value interface{}, currency string) string)
+		price := priceFunc(value, currency)
+		currencyLabel = pff.TranslationService.Translate(currencyLabel, "", "", 1, nil)
+		format, ok := pff.Config["formatLong"].(string)
+		if ok {
+			return fmt.Sprintf(format, price, currencyLabel)
+		}
+		return price
+	}
+}

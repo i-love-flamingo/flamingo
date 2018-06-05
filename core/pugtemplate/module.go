d3c826fc4aeb100c34735d1a12eab56f176ce7f2
Thomas Layh <thomas.layh@aoe.com>
2018-06-05 14:51:57 +0200

    TASK - delete url escape function

--- core/pugtemplate/module.go
+++ core/pugtemplate/module.go
@@ -91,7 +91,6 @@ func (m *Module) Configure(injector *dingo.Injector) {
 
 	injector.BindMulti((*template.ContextFunction)(nil)).To(templatefunctions.URLFunc{})
 	injector.BindMulti((*template.ContextFunction)(nil)).To(templatefunctions.TryURLFunc{})
-	injector.BindMulti((*template.ContextFunction)(nil)).To(templatefunctions.URLEscapeFunc{})
 	injector.BindMulti((*template.ContextFunction)(nil)).To(templatefunctions.GetFunc{})
 	injector.BindMulti((*template.ContextFunction)(nil)).To(templatefunctions.DataFunc{})
 	injector.BindMulti((*template.Function)(nil)).To(templatefunctions.StriptagsFunc{})

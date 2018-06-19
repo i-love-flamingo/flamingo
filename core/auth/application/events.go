042b7e9bdff1cc86ef6949b55295e7321e8e460f
Bastian Ike <bastian.ike@aoe.com>
2018-06-19 10:41:16 +0200

    core/auth: fix go lint issues

--- core/auth/application/events.go
+++ core/auth/application/events.go
@@ -6,16 +6,17 @@ import (
 )
 
 type (
-	// EventPublisher
-	EventPublisher struct {
-	}
+	// EventPublisher struct
+	EventPublisher struct{}
 )
 
+// PublishLoginEvent dispatches the login event on the contexts event router
 func (e *EventPublisher) PublishLoginEvent(ctx web.Context, event *domain.LoginEvent) {
 	//publish to Flamingo default Event Router
 	ctx.EventRouter().Dispatch(ctx, event)
 }
 
+// PublishLogoutEvent dispatches the logout event on the contexts event router
 func (e *EventPublisher) PublishLogoutEvent(ctx web.Context, event *domain.LogoutEvent) {
 	//publish to Flamingo default Event Router
 	ctx.EventRouter().Dispatch(ctx, event)

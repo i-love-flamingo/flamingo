eeb86f269213c793f53f48d7f02ae87aa074bc44
Bastian Ike <bastian.ike@aoe.com>
2018-06-19 09:08:59 +0200

    framework/router: rewrite router
    the router now supports mehtod-agnostic routing
    and an improved routing model

--- framework/router/controllers.go
+++ framework/router/controllers.go
@@ -1,11 +1,21 @@
 package router
 
-import "flamingo.me/flamingo/framework/web"
+import (
+	"context"
+	"net/http"
+
+	"flamingo.me/flamingo/framework/web"
+)
 
 type (
+	// Action defines an explicit http action
+	Action func(ctx context.Context, req *web.Request) web.Response
+
 	// Controller defines a web Controller
 	// it is an interface{} as it can be served by multiple possible controllers,
 	// such as generic GET/POST Controller, http.Handler, Handler-functions, etc.
+	//
+	// deprecated: superseeded by method based handling
 	Controller interface{}
 
 	// ControllerOption defines a type for Controller options
@@ -17,30 +27,35 @@ type (
 	}
 
 	// GETController is implemented by controllers which have a Get method
+	// deprecated: use method based handler
 	GETController interface {
 		// Get is called for GET-Requests
 		Get(web.Context) web.Response
 	}
 
 	// POSTController is implemented by controllers which have a Post method
+	// deprecated: use method based handler
 	POSTController interface {
 		// Post is called for POST-Requests
 		Post(web.Context) web.Response
 	}
 
 	// PUTController is implemented by controllers which have a Put method
+	// deprecated: use method based handler
 	PUTController interface {
 		// Put is called for PUT-Requests
 		Put(web.Context) web.Response
 	}
 
 	// DELETEController is implemented by controllers which have a Delete method
+	// deprecated: use method based handler
 	DELETEController interface {
 		// Delete is called for DELETE-Requests
 		Delete(web.Context) web.Response
 	}
 
 	// HEADController is implemented by controllers which have a Head method
+	// deprecated: use method based handler
 	HEADController interface {
 		// Head is called for HEAD-Requests
 		Head(web.Context) web.Response
@@ -58,3 +73,14 @@ type (
 	// DataHandler behaves the same as DataController, but just for direct callbacks
 	DataHandler func(web.Context) interface{}
 )
+
+// HttpAction wraps a default http.Handler to a flamingo router action
+func HttpAction(handler http.Handler) Action {
+	return func(ctx context.Context, req *web.Request) web.Response {
+		r := &web.ServeHTTPResponse{
+			VerboseResponseWriter: ctx.Value("rw").(*web.VerboseResponseWriter),
+		}
+		handler.ServeHTTP(r, req.Request())
+		return r
+	}
+}
--- framework/router/router_test.go
+++ framework/router/router_test.go
@@ -10,18 +10,157 @@ import (
 	"net/url"
 	"testing"
 
-	"github.com/gojuno/minimock"
-	"github.com/gorilla/sessions"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
-	"github.com/stretchr/testify/require"
 	"flamingo.me/flamingo/framework/event"
 	eventMocks "flamingo.me/flamingo/framework/event/mocks"
 	"flamingo.me/flamingo/framework/profiler"
 	profilerMocks "flamingo.me/flamingo/framework/profiler/mocks"
 	"flamingo.me/flamingo/framework/web"
 	webMocks "flamingo.me/flamingo/framework/web/mocks"
+	"github.com/gojuno/minimock"
+	"github.com/gorilla/sessions"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
 )
+
+func TestRouter(t *testing.T) {
+	router := new(Router)
+
+	profilerMock := new(profilerMocks.Profiler)
+	router.ProfilerProvider = func() profiler.Profiler { return profilerMock }
+
+	registry := NewRegistry()
+	router.RouterRegistry = registry
+
+	contextMock := new(webMocks.Context)
+	router.ContextFactory = func(p profiler.Profiler, e event.Router, rw http.ResponseWriter, r *http.Request, session *sessions.Session) web.Context {
+		contextMock.On("Profile", "matchRequest", "/test").Return(profiler.ProfileFinishFunc(func() {}))
+		contextMock.On("Profile", "request", "/test").Return(profiler.ProfileFinishFunc(func() {}))
+		contextMock.On("LoadParams", mock.Anything)
+		contextMock.On(
+			"WithValue",
+			mock.MatchedBy(func(key interface{}) bool { return key.(string) == "Handler" }),
+			mock.Anything,
+		).Return(contextMock)
+		contextMock.On(
+			"WithValue",
+			mock.MatchedBy(func(key interface{}) bool { return key.(string) == "HandlerName" }),
+			mock.Anything,
+		).Return(contextMock)
+		return contextMock
+	}
+	eventRouter := new(eventMocks.Router)
+	eventRouter.On("Dispatch", mock.Anything, mock.Anything)
+	router.eventrouter = eventRouter
+
+	server := httptest.NewServer(router)
+	defer server.Close()
+
+	testReq := func(method, path string) error {
+		request, err := http.NewRequest(method, path, nil)
+		assert.NoError(t, err)
+		request.URL, err = url.Parse(server.URL + path)
+		assert.NoError(t, err)
+
+		res, err := http.DefaultClient.Do(request)
+		if err != nil {
+			return err
+		}
+		return res.Body.Close()
+	}
+
+	var method string
+
+	t.Run("Test Legacy Fallback", func(t *testing.T) {
+		registry.Route("/test", "test")
+		registry.Handle("test", func(context web.Context) web.Response { method = "Handle"; return nil })
+
+		method = ""
+		assert.NoError(t, testReq(http.MethodGet, "/test"))
+		assert.Equal(t, "Handle", method)
+		method = ""
+		assert.NoError(t, testReq(http.MethodPost, "/test"))
+		assert.Equal(t, "Handle", method)
+		method = ""
+		assert.NoError(t, testReq(http.MethodHead, "/test"))
+		assert.Equal(t, "Handle", method)
+		method = ""
+		assert.NoError(t, testReq(http.MethodPut, "/test"))
+		assert.Equal(t, "Handle", method)
+		method = ""
+		assert.NoError(t, testReq(http.MethodOptions, "/test"))
+		assert.Equal(t, "Handle", method)
+		method = ""
+		assert.NoError(t, testReq(http.MethodDelete, "/test"))
+		assert.Equal(t, "Handle", method)
+		method = ""
+		assert.NoError(t, testReq("CUSTOM", "/test"))
+		assert.Equal(t, "Handle", method)
+	})
+
+	t.Run("Test Any", func(t *testing.T) {
+		registry.Route("/test", "test")
+		registry.HandleAny("test", func(context context.Context, req *web.Request) web.Response { method = "HandleAny"; return nil })
+
+		method = ""
+		assert.NoError(t, testReq(http.MethodGet, "/test"))
+		assert.Equal(t, "HandleAny", method)
+		method = ""
+		assert.NoError(t, testReq(http.MethodPost, "/test"))
+		assert.Equal(t, "HandleAny", method)
+		method = ""
+		assert.NoError(t, testReq(http.MethodHead, "/test"))
+		assert.Equal(t, "HandleAny", method)
+		method = ""
+		assert.NoError(t, testReq(http.MethodPut, "/test"))
+		assert.Equal(t, "HandleAny", method)
+		method = ""
+		assert.NoError(t, testReq(http.MethodOptions, "/test"))
+		assert.Equal(t, "HandleAny", method)
+		method = ""
+		assert.NoError(t, testReq(http.MethodDelete, "/test"))
+		assert.Equal(t, "HandleAny", method)
+		method = ""
+		assert.NoError(t, testReq("CUSTOM", "/test"))
+		assert.Equal(t, "HandleAny", method)
+	})
+
+	t.Run("Test HTTP Methods", func(t *testing.T) {
+		registry.Route("/test", "test")
+		registry.HandleGet("test", func(context context.Context, req *web.Request) web.Response { method = "HandleGet"; return nil })
+		registry.HandlePost("test", func(context context.Context, req *web.Request) web.Response { method = "HandlePost"; return nil })
+		registry.HandlePut("test", func(context context.Context, req *web.Request) web.Response { method = "HandlePut"; return nil })
+		registry.HandleOptions("test", func(context context.Context, req *web.Request) web.Response { method = "HandleOptions"; return nil })
+		registry.HandleHead("test", func(context context.Context, req *web.Request) web.Response { method = "HandleHead"; return nil })
+		registry.HandleDelete("test", func(context context.Context, req *web.Request) web.Response { method = "HandleDelete"; return nil })
+		registry.HandleMethod("CUSTOM", "test", func(context context.Context, req *web.Request) web.Response { method = "HandleCustom"; return nil })
+
+		method = ""
+		assert.NoError(t, testReq(http.MethodGet, "/test"))
+		assert.Equal(t, "HandleGet", method)
+		method = ""
+		assert.NoError(t, testReq(http.MethodPost, "/test"))
+		assert.Equal(t, "HandlePost", method)
+		method = ""
+		assert.NoError(t, testReq(http.MethodHead, "/test"))
+		assert.Equal(t, "HandleHead", method)
+		method = ""
+		assert.NoError(t, testReq(http.MethodPut, "/test"))
+		assert.Equal(t, "HandlePut", method)
+		method = ""
+		assert.NoError(t, testReq(http.MethodOptions, "/test"))
+		assert.Equal(t, "HandleOptions", method)
+		method = ""
+		assert.NoError(t, testReq(http.MethodDelete, "/test"))
+		assert.Equal(t, "HandleDelete", method)
+		method = ""
+		assert.NoError(t, testReq("CUSTOM", "/test"))
+		assert.Equal(t, "HandleCustom", method)
+		method = ""
+		assert.NoError(t, testReq("UNASSIGNED", "/test"))
+		assert.Equal(t, "HandleAny", method)
+	})
+}
 
 // Example
 func TestTest(t *testing.T) {

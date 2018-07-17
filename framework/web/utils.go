ce7281017227d1378cddaa4cd28c30b78c87122e
Bastian Ike <bastian.ike@aoe.com>
2018-07-17 09:46:00 +0200

    framework: helper to easier use new web request and context

--- framework/web/utils.go
+++ framework/web/utils.go
@@ -1,6 +1,7 @@
 package web
 
 import (
+	"context"
 	"strings"
 )
 
@@ -14,4 +15,16 @@ func URLTitle(title string) string {
 	}
 
 	return url
+}
+
+// ToContext supports legacy usage of the web.Context type
+func ToContext(ctx context.Context) Context {
+	return ctx.Value(CONTEXT).(Context)
+}
+
+// ToRequest upgrades a web.Context to the new context+request form
+func ToRequest(ctx Context) (context.Context, *Request) {
+	request := ctx.Value("__req").(*Request)
+
+	return request.request.Context(), request
 }

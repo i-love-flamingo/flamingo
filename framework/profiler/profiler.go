a49ee2807aff2c5905daf625bd4ec57b1cfbf984
Bastian Ike <bastian.ike@aoe.com>
2018-07-22 11:08:23 +0200

    framework: mark profiler as deprecated

--- framework/profiler/profiler.go
+++ framework/profiler/profiler.go
@@ -2,10 +2,13 @@ package profiler
 
 type (
 	// ProfileFinishFunc is used to finish a profiling-block
+	// deprecated: use opencensus
 	ProfileFinishFunc func()
 
 	// Profiler is used to measure the time certain things need
+	// deprecated: use opencensus
 	Profiler interface {
+		// deprecated: use opencensus
 		Profile(key, msg string) ProfileFinishFunc
 	}
 )

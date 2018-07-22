76421bb39b25c9edc665c2e1be4df54633d17d69
Bastian Ike <bastian.ike@aoe.com>
2018-07-22 16:27:50 +0200

    framework: fix context.Context to web.Context legacy conversion

--- framework/web/utils.go
+++ framework/web/utils.go
@@ -3,6 +3,8 @@ package web
 import (
 	"context"
 	"strings"
+
+	"github.com/pkg/errors"
 )
 
 // URLTitle normalizes a title for nice usage in URLs
@@ -26,13 +28,18 @@ func ToContext(ctx context.Context) Context {
 	if c, ok := ctx.Value(CONTEXT).(Context); ok {
 		return c
 	}
-	return nil
+
+	panic(errors.New("can not convert a context.Context to a web.Context"))
 }
 
 // ToRequest upgrades a web.Context to the new context+request form
 // deprecated: always use context+request directly
 func ToRequest(ctx Context) (context.Context, *Request) {
-	request := ctx.Value("__req").(*Request)
+	request, ok := ctx.Value("__req").(*Request)
+
+	if !ok {
+		return ctx, RequestFromRequest(ctx.Request(), ctx.Session())
+	}
 
 	return request.request.Context(), request
 }

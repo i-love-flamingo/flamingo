e1e00d176b5b9f04e5f6e899a8f43a610919d708
Bastian Ike <bastian.ike@aoe.com>
2018-07-26 14:39:30 +0200

    framework/opencensus: stats exporter

--- framework/opencensus/module.go
+++ framework/opencensus/module.go
@@ -8,33 +8,63 @@ import (
 	"flamingo.me/flamingo/framework/config"
 	"flamingo.me/flamingo/framework/dingo"
 	"go.opencensus.io/exporter/jaeger"
+	"go.opencensus.io/exporter/prometheus"
 	"go.opencensus.io/plugin/ochttp"
+	"go.opencensus.io/stats"
+	"go.opencensus.io/stats/view"
+	"go.opencensus.io/tag"
 	"go.opencensus.io/trace"
 )
 
-var registerOnce = new(sync.Once)
+var (
+	registerOnce = new(sync.Once)
+	KeyArea, _   = tag.NewKey("area")
+)
 
-type Module struct {
-	Endpoint    string `inject:"config:opencensus.jaeger.endpoint"`
-	ServiceName string `inject:"config:opencensus.serviceName"`
+func View(m stats.Measure, aggr *view.Aggregation, tagKeys ...tag.Key) {
+	view.Register(&view.View{
+		Measure:     m,
+		Aggregation: aggr,
+		TagKeys:     append([]tag.Key{KeyArea}, tagKeys...),
+	})
 }
 
-func (m *Module) Configure(*dingo.Injector) {
+type Module struct {
+	Endpoint     string `inject:"config:opencensus.jaeger.endpoint"`
+	ServiceName  string `inject:"config:opencensus.serviceName"`
+	ServiceAddr  string `inject:"config:opencensus.serviceAddr"`
+	JaegerEnable bool   `inject:"config:opencensus.jaeger.enable"`
+}
+
+func (m *Module) Configure(injector *dingo.Injector) {
 	registerOnce.Do(func() {
 		// For demoing purposes, always sample.
 		trace.ApplyConfig(trace.Config{DefaultSampler: trace.AlwaysSample()})
 		http.DefaultTransport = &ochttp.Transport{Base: http.DefaultTransport}
 
-		// Register the Jaeger exporter to be able to retrieve
-		// the collected spans.
-		exporter, err := jaeger.NewExporter(jaeger.Options{
-			Endpoint:    m.Endpoint,
-			ServiceName: m.ServiceName,
-		})
-		if err != nil {
-			log.Fatal(err)
+		if m.JaegerEnable {
+			// Register the Jaeger exporter to be able to retrieve
+			// the collected spans.
+			exporter, err := jaeger.NewExporter(jaeger.Options{
+				Endpoint:    m.Endpoint,
+				ServiceName: m.ServiceName,
+			})
+			if err != nil {
+				log.Fatal(err)
+			}
+			trace.RegisterExporter(exporter)
 		}
-		trace.RegisterExporter(exporter)
+
+		{
+			exporter, err := prometheus.NewExporter(prometheus.Options{})
+			if err != nil {
+				log.Fatal(err)
+			}
+			view.RegisterExporter(exporter)
+			s := http.NewServeMux()
+			s.Handle("/metrics", exporter)
+			go http.ListenAndServe(m.ServiceAddr, s)
+		}
 	})
 }
 
@@ -42,7 +72,9 @@ func (m *Module) DefaultConfig() config.Map {
 	return config.Map{
 		"opencensus": config.Map{
 			"jaeger.endpoint": "http://localhost:14268",
+			"jaeger.enable":   false,
 			"serviceName":     "flamingo",
+			"serviceAddr":     ":13210",
 		},
 	}
 }

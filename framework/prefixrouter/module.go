24504228f75f0ecae8153270c56d9161921ed4f5
Thorsten Essig <thorsten.essig@aoe.com>
2018-08-15 16:11:10 +0200

    framework/prefixrouter: add graceful shutdown via event

--- framework/prefixrouter/module.go
+++ framework/prefixrouter/module.go
@@ -1,12 +1,15 @@
 package prefixrouter
 
 import (
+	"context"
 	"net/http"
 	"net/url"
 	"strings"
+	"time"
 
 	"flamingo.me/flamingo/framework/config"
 	"flamingo.me/flamingo/framework/dingo"
+	"flamingo.me/flamingo/framework/event"
 	"flamingo.me/flamingo/framework/flamingo"
 	"flamingo.me/flamingo/framework/router"
 	"github.com/spf13/cobra"
@@ -14,34 +17,47 @@ import (
 )
 
 // Module for core/prefix_router
-type Module struct{}
+type Module struct {
+	server *http.Server
+	logger flamingo.Logger
+}
 
 // Configure DI
 func (m *Module) Configure(injector *dingo.Injector) {
 	injector.Bind((*http.ServeMux)(nil)).ToInstance(http.NewServeMux())
-	injector.BindMulti(new(cobra.Command)).ToProvider(serveCmd)
+	injector.BindMulti(new(cobra.Command)).ToProvider(serveCmd(m))
+	injector.BindMulti((*event.Subscriber)(nil)).ToInstance(m)
 }
 
-func serveCmd(area *config.Area, logger flamingo.Logger, defaultmux *http.ServeMux, config *struct {
-	PrimaryHandlers  []OptionalHandler `inject:"primaryHandlers,optional"` //Optional Register a PrimaryHandlersHandlers which is passed to the FrontendRouter
-	FallbackHandlers []OptionalHandler `inject:"fallback,optional"`        //Optional Register a FallbackHandlers which is passed to the FrontendRouter
+func (m *Module) Inject(l flamingo.Logger) {
+	m.logger = l
+}
+
+func serveCmd(m *Module) func(area *config.Area, defaultmux *http.ServeMux, config *struct {
+	PrimaryHandlers  []OptionalHandler `inject:"primaryHandlers,optional"` // Optional Register a PrimaryHandlersHandlers which is passed to the FrontendRouter
+	FallbackHandlers []OptionalHandler `inject:"fallback,optional"`        // Optional Register a FallbackHandlers which is passed to the FrontendRouter
 }) *cobra.Command {
-	var addr string
+	return func(area *config.Area, defaultmux *http.ServeMux, config *struct {
+		PrimaryHandlers  []OptionalHandler `inject:"primaryHandlers,optional"` // Optional Register a PrimaryHandlersHandlers which is passed to the FrontendRouter
+		FallbackHandlers []OptionalHandler `inject:"fallback,optional"`        // Optional Register a FallbackHandlers which is passed to the FrontendRouter
+	}) *cobra.Command {
+		var addr string
 
-	cmd := &cobra.Command{
-		Use:     "serve",
-		Short:   "run the prefix router",
-		Aliases: []string{"server"},
-		Run:     serve(area, defaultmux, &addr, config.PrimaryHandlers, config.FallbackHandlers, logger),
+		cmd := &cobra.Command{
+			Use:     "serve",
+			Short:   "run the prefix router",
+			Aliases: []string{"server"},
+			Run:     m.serve(area, defaultmux, &addr, config.PrimaryHandlers, config.FallbackHandlers),
+		}
+
+		cmd.Flags().StringVarP(&addr, "addr", "a", ":3210", "addr on which flamingo runs")
+
+		return cmd
 	}
-
-	cmd.Flags().StringVarP(&addr, "addr", "a", ":3210", "addr on which flamingo runs")
-
-	return cmd
 }
 
 // serve HTTP Requests
-func serve(root *config.Area, defaultRouter *http.ServeMux, addr *string, primaryHandlers []OptionalHandler, fallbackHandlers []OptionalHandler, logger flamingo.Logger) func(cmd *cobra.Command, args []string) {
+func (m *Module) serve(root *config.Area, defaultRouter *http.ServeMux, addr *string, primaryHandlers []OptionalHandler, fallbackHandlers []OptionalHandler) func(cmd *cobra.Command, args []string) {
 	return func(cmd *cobra.Command, args []string) {
 		frontRouter := NewFrontRouter()
 		frontRouter.SetFinalFallbackHandler(defaultRouter)
@@ -52,7 +68,7 @@ func serve(root *config.Area, defaultRouter *http.ServeMux, addr *string, primar
 			baseurlVal, ok := area.Configuration.Get("prefixrouter.baseurl")
 
 			if !ok {
-				logger.WithField("category", "prefixrouter").Warn("No baseurl configured for config area %v", area.Name)
+				m.logger.WithField("category", "prefixrouter").Warn("No baseurl configured for config area %v", area.Name)
 				continue
 			}
 
@@ -62,8 +78,8 @@ func serve(root *config.Area, defaultRouter *http.ServeMux, addr *string, primar
 				baseurl = "host" + baseurl
 			}
 
-			logger.WithField("category", "prefixrouter").Info("Routing", area.Name, "at", baseurl)
-			area.Injector.Bind((*flamingo.Logger)(nil)).ToInstance(logger.WithField("area", area.Name))
+			m.logger.WithField("category", "prefixrouter").Info("Routing ", area.Name, " at ", baseurl)
+			area.Injector.Bind((*flamingo.Logger)(nil)).ToInstance(m.logger.WithField("area", area.Name))
 			areaRouter := area.Injector.GetInstance(router.Router{}).(*router.Router)
 			areaRouter.Init(area)
 			bu, _ := url.Parse("scheme://" + baseurl)
@@ -72,11 +88,29 @@ func serve(root *config.Area, defaultRouter *http.ServeMux, addr *string, primar
 			frontRouter.Add(bu.Path, routerHandler{area: area.Name, handler: areaRouter})
 		}
 
-		logger.Info("Starting HTTP Server at %s .....", *addr)
+		m.logger.WithField("category", "prefixrouter").Info("Starting HTTP Server at ", *addr, ".....")
+		m.server = &http.Server{
+			Addr:    *addr,
+			Handler: &ochttp.Handler{IsPublicEndpoint: true, Handler: frontRouter},
+		}
 
-		e := http.ListenAndServe(*addr, &ochttp.Handler{IsPublicEndpoint: true, Handler: frontRouter})
-		if e != nil {
-			logger.Error("Unexpected Error", e)
+		e := m.server.ListenAndServe()
+		if e != nil && e != http.ErrServerClosed {
+			m.logger.WithField("category", "prefixrouter").Error("Unexpected Error ", e)
+		}
+	}
+}
+
+func (m *Module) Notify(event event.Event) {
+	time.Sleep(50 * time.Second)
+	switch event.(type) {
+	case *flamingo.AppShutdownEvent:
+		ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+		m.logger.WithField("category", "prefixrouter").Info("Shutdown server on ", m.server.Addr)
+
+		err := m.server.Shutdown(ctx)
+		if err != nil {
+			m.logger.WithField("category", "prefixrouter").Error("unexpected error on server shutdown: ", err)
 		}
 	}
 }

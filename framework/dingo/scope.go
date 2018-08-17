6a7a11a13542ee28b7521f5636e79e5eaf408601
Bastian Ike <bastian.ike@aoe.com>
2018-08-17 13:28:51 +0200

    framework/dingo: proper singleton implementation
    the previous implementation was prune to deadlocks.
    now cross-routine synchronisation should work just fine and make sure
    that singletons are created only once, without deadlocking.

--- framework/dingo/scope.go
+++ framework/dingo/scope.go
@@ -11,12 +11,17 @@ type (
 		ResolveType(t reflect.Type, annotation string, unscoped func(t reflect.Type, annotation string, optional bool) reflect.Value) reflect.Value
 	}
 
+	identifier struct {
+		t reflect.Type
+		a string
+	}
+
 	// SingletonScope is our Scope to handle Singletons
 	// todo use RWMutex for proper locking
 	SingletonScope struct {
-		sync.Mutex
-		instanceLock map[reflect.Type]*sync.Mutex
-		instances    map[reflect.Type]map[string]reflect.Value
+		sync.Mutex                                // lock guarding instaceLocks
+		instanceLock map[identifier]*sync.RWMutex // lock guarding instances
+		instances    map[identifier]reflect.Value
 	}
 
 	// ChildSingletonScope manages child-specific singleton
@@ -25,74 +30,46 @@ type (
 
 var (
 	// Singleton is the default SingletonScope for dingo
-	Singleton Scope = new(SingletonScope)
+	Singleton Scope = NewSingletonScope()
 
 	// ChildSingleton is a per-child singleton
-	ChildSingleton Scope = new(ChildSingletonScope)
+	ChildSingleton Scope = NewChildSingletonScope()
 )
+
+// NewSingletonScope creates a new singleton scope
+func NewSingletonScope() *SingletonScope {
+	return &SingletonScope{instanceLock: make(map[identifier]*sync.RWMutex), instances: make(map[identifier]reflect.Value)}
+}
+
+// NewChildSingletonScope creates a new child singleton scope
+func NewChildSingletonScope() *ChildSingletonScope {
+	return &ChildSingletonScope{instanceLock: make(map[identifier]*sync.RWMutex), instances: make(map[identifier]reflect.Value)}
+}
 
 // ResolveType resolves a request in this scope
 func (s *SingletonScope) ResolveType(t reflect.Type, annotation string, unscoped func(t reflect.Type, annotation string, optional bool) reflect.Value) reflect.Value {
-	// we got one :)
-	// todo: read lock?
-	if found, ok := s.instances[t]; ok {
-		if found, ok := found[annotation]; ok {
-			return found
-		}
-	}
+	ident := identifier{t, annotation}
 
-	// without an existing instance we need to create one
-
-	// Lock ourselve
+	// try to get the instance type lock
 	s.Lock()
 
-	// someone already built our instance while we were waiting for the lock
-	if found, ok := s.instances[t]; ok {
-		if found, ok := found[annotation]; ok {
-			s.Unlock()
-			return found
-		}
+	if l, ok := s.instanceLock[ident]; ok {
+		// we have the instance lock
+		s.Unlock()
+		l.RLock()
+		defer l.RUnlock()
+		return s.instances[ident]
 	}
 
-	// If instanceLock is empty, create it now
-	if s.instanceLock == nil {
-		s.instanceLock = make(map[reflect.Type]*sync.Mutex)
-	}
-
-	// check for the concrete instanceLock
-	if s.instanceLock[t] == nil {
-		s.instanceLock[t] = new(sync.Mutex)
-	}
-
-	// acquire the instance-type's lock
-	s.instanceLock[t].Lock()
-	defer s.instanceLock[t].Unlock()
-
-	// someone already built our instance while we were waiting/setup the locks
-	if found, ok := s.instances[t]; ok {
-		if found, ok := found[annotation]; ok {
-			return found
-		}
-	}
-
-	if s.instances == nil {
-		s.instances = make(map[reflect.Type]map[string]reflect.Value)
-	}
-
-	if s.instances[t] == nil {
-		s.instances[t] = make(map[string]reflect.Value)
-	}
-
-	// release our main lock so we won't lock ourselves when trying to create a singleton
-	// with a singleton dependency
+	s.instanceLock[ident] = new(sync.RWMutex)
+	s.instanceLock[ident].Lock()
 	s.Unlock()
 
-	// save our new generated singleton
-	// todo race condition?
-	s.instances[t][annotation] = unscoped(t, annotation, false)
+	s.instances[ident] = unscoped(t, annotation, false)
 
-	// return the new singleton
-	return s.instances[t][annotation]
+	s.instanceLock[ident].Unlock()
+
+	return s.instances[ident]
 }
 
 // ResolveType delegates to SingletonScope.ResolveType
--- framework/dingo/scope_test.go
+++ framework/dingo/scope_test.go
@@ -1,6 +1,7 @@
 package dingo
 
 import (
+	"fmt"
 	"reflect"
 	"sync"
 	"sync/atomic"
@@ -13,6 +14,7 @@ func testScope(t *testing.T, scope Scope) {
 	var requestedUnscoped int64
 
 	test := reflect.TypeOf("string")
+	test2 := reflect.TypeOf("int")
 
 	unscoped := func(t reflect.Type, annotation string, optional bool) reflect.Value {
 		atomic.AddInt64(&requestedUnscoped, 1)
@@ -23,33 +25,74 @@ func testScope(t *testing.T, scope Scope) {
 		return reflect.New(t).Elem()
 	}
 
-	runs := 1 // change to 10? 100? 1000? to trigger a bug? todo investigate
+	runs := 100 // change to 10? 100? 1000? to trigger a bug? todo investigate
 
 	wg := new(sync.WaitGroup)
 	wg.Add(runs)
 	for i := 0; i < runs; i++ {
 		go func() {
 			t1 := scope.ResolveType(test, "", unscoped)
+			t12 := scope.ResolveType(test2, "", unscoped)
 			t2 := scope.ResolveType(test, "", unscoped)
+			t22 := scope.ResolveType(test2, "", unscoped)
 			assert.Equal(t, t1, t2)
+			assert.Equal(t, t12, t22)
 			wg.Done()
 		}()
 	}
 	wg.Wait()
 
 	assert.Equal(t, int64(1), requestedUnscoped)
+
 }
 
 func TestSingleton_ResolveType(t *testing.T) {
 	// reset instance
-	Singleton = new(SingletonScope)
+	Singleton = NewSingletonScope()
 
 	testScope(t, Singleton)
 }
 
 func TestChildSingleton_ResolveType(t *testing.T) {
 	// reset instance
-	ChildSingleton = new(ChildSingletonScope)
+	ChildSingleton = NewChildSingletonScope()
 
 	testScope(t, ChildSingleton)
+}
+
+type (
+	singletonA struct {
+		B *singletonB `inject:""`
+	}
+
+	singletonB struct {
+		C singletonC `inject:""`
+	}
+
+	singletonC string
+)
+
+func TestScopeWithSubDependencies(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		t.Run(fmt.Sprintf("Run %d", i), func(t *testing.T) {
+			injector := NewInjector()
+
+			injector.Bind(new(singletonA)).In(Singleton)
+			injector.Bind(new(singletonB)).In(Singleton)
+			injector.Bind(singletonC("")).In(Singleton).ToInstance(singletonC("singleton C"))
+
+			runs := 10
+
+			wg := new(sync.WaitGroup)
+			wg.Add(runs)
+			for i := 0; i < runs; i++ {
+				go func() {
+					a := injector.GetInstance(new(singletonA)).(*singletonA)
+					assert.Equal(t, a.B.C, singletonC("singleton C"))
+					wg.Done()
+				}()
+			}
+			wg.Wait()
+		})
+	}
 }

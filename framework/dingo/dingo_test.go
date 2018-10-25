54770ab1321e749ab53072bf42565568e3e8f624
Bastian Ike <bastian.ike@aoe.com>
2018-10-25 08:39:40 +0200

    dingo: cleanup test

--- framework/dingo/dingo_test.go
+++ framework/dingo/dingo_test.go
@@ -75,7 +75,43 @@ func (if2 *InterfaceImpl2) Test() int {
 	return 2
 }
 
-// AOP
+func TestDingoResolving(t *testing.T) {
+	t.Run("Should resolve dependencies on request", func(t *testing.T) {
+		injector := NewInjector(new(PreTestModule), new(TestModule))
+
+		var iface Interface
+		iface = injector.GetInstance(new(Interface)).(Interface)
+
+		assert.Equal(t, 1, iface.Test())
+
+		dt := *injector.GetInstance(new(DepTest)).(*DepTest)
+
+		assert.Equal(t, 1, dt.Iface.Test())
+		assert.Equal(t, 2, dt.Iface2.Test())
+
+		var dt2 DepTest
+		injector.requestInjection(&dt2, nil)
+
+		assert.Equal(t, 1, dt2.Iface.Test())
+		assert.Equal(t, 2, dt2.Iface2.Test())
+
+		assert.Equal(t, 1, dt.IfaceProvided.Test())
+		assert.Equal(t, 1, dt.IfaceImpl1Provided.Test())
+		assert.Equal(t, 2, dt.IfaceInstance.Test())
+
+		assert.Equal(t, 1, dt.IfaceProvider().Test())
+		assert.Equal(t, "Hello World", dt.IfaceProvided.(*InterfaceImpl1).foo)
+		assert.Equal(t, "Hello World", dt.IfaceImpl1Provided.(*InterfaceImpl1).foo)
+	})
+
+	t.Run("Should resolve scopes", func(t *testing.T) {
+		injector := NewInjector(new(TestModule))
+
+		assert.Equal(t, injector.GetInstance(TestSingleton{}), injector.GetInstance(TestSingleton{}))
+	})
+}
+
+// interceptors
 type (
 	AopInterface interface {
 		Test() string
@@ -117,55 +153,13 @@ func (a *AopInterceptor2) Test() string {
 	return a.AopInterface.Test() + " 2"
 }
 
-func TestDingo(t *testing.T) {
-	t.Run("Dingo Test", func(t *testing.T) {
-		t.Run("Simple resolve", func(t *testing.T) {
-			t.Run("Should resolve dependencies on request", func(t *testing.T) {
-				injector := NewInjector(new(PreTestModule), new(TestModule))
+func TestInterceptors(t *testing.T) {
+	injector := NewInjector(new(AopModule))
 
-				var iface Interface
-				iface = injector.GetInstance(new(Interface)).(Interface)
+	var dep AopDep
+	injector.requestInjection(&dep, nil)
 
-				assert.Equal(t, 1, iface.Test())
-
-				dt := *injector.GetInstance(new(DepTest)).(*DepTest)
-
-				assert.Equal(t, 1, dt.Iface.Test())
-				assert.Equal(t, 2, dt.Iface2.Test())
-
-				var dt2 DepTest
-				injector.requestInjection(&dt2, nil)
-
-				assert.Equal(t, 1, dt2.Iface.Test())
-				assert.Equal(t, 2, dt2.Iface2.Test())
-
-				assert.Equal(t, 1, dt.IfaceProvided.Test())
-				assert.Equal(t, 1, dt.IfaceImpl1Provided.Test())
-				assert.Equal(t, 2, dt.IfaceInstance.Test())
-
-				assert.Equal(t, 1, dt.IfaceProvider().Test())
-				assert.Equal(t, "Hello World", dt.IfaceProvided.(*InterfaceImpl1).foo)
-				assert.Equal(t, "Hello World", dt.IfaceImpl1Provided.(*InterfaceImpl1).foo)
-			})
-
-			t.Run("Should resolve scopes", func(t *testing.T) {
-				injector := NewInjector(new(TestModule))
-
-				assert.Equal(t, injector.GetInstance(TestSingleton{}), injector.GetInstance(TestSingleton{}))
-			})
-		})
-
-		t.Run("AOP", func(t *testing.T) {
-			t.Run("Should intercept interfaces", func(t *testing.T) {
-				injector := NewInjector(new(AopModule))
-
-				var dep AopDep
-				injector.requestInjection(&dep, nil)
-
-				assert.Equal(t, "Test 1 2", dep.A.Test())
-			})
-		})
-	})
+	assert.Equal(t, "Test 1 2", dep.A.Test())
 }
 
 func TestOptional(t *testing.T) {

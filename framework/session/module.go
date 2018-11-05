1373acb7ffaa8b858aa223e290cf31caa74c7329
Bastian Ike <bastian.ike@aoe.com>
2018-11-05 14:48:58 +0100

    session store configuration options

--- framework/session/module.go
+++ framework/session/module.go
@@ -20,9 +20,11 @@ type Module struct {
 	// float64 is used due to the injection as config from json - int is not possible on this
 	StoreLength          float64 `inject:"config:session.store.length"`
 	MaxAge               float64 `inject:"config:session.max.age"`
+	Path                 string  `inject:"config:session.cookie.path"`
 	RedisHost            string  `inject:"config:session.redis.host"`
 	RedisPassword        string  `inject:"config:session.redis.password"`
 	RedisIdleConnections float64 `inject:"config:session.redis.idle.connections"`
+	RedisMaxAge          float64 `inject:"config:session.redis.maxAge"`
 }
 
 // Configure DI
@@ -31,27 +33,37 @@ func (m *Module) Configure(injector *dingo.Injector) {
 	case "redis":
 		sessionStore, err := redistore.NewRediStore(int(m.RedisIdleConnections), "tcp", m.RedisHost, m.RedisPassword, []byte(m.Secret))
 		if err != nil {
-			panic(err)
+			panic(err) // todo: don't panic? fallback?
 		}
+
 		sessionStore.SetMaxAge(int(m.MaxAge))
 		sessionStore.SetMaxLength(int(m.StoreLength))
 		sessionStore.Options.Secure = m.Secure
 		sessionStore.Options.HttpOnly = true
+		sessionStore.Options.Path = m.Path
+		sessionStore.DefaultMaxAge = int(m.RedisMaxAge)
+
 		injector.Bind((*sessions.Store)(nil)).ToInstance(sessionStore)
 	case "file":
 		os.Mkdir(m.FileName, os.ModePerm)
 		sessionStore := sessions.NewFilesystemStore(m.FileName, []byte(m.Secret))
+
 		sessionStore.MaxLength(int(m.StoreLength))
 		sessionStore.MaxAge(int(m.MaxAge))
 		sessionStore.Options.Secure = m.Secure
 		sessionStore.Options.HttpOnly = true
+		sessionStore.Options.Path = m.Path
+
 		injector.Bind((*sessions.Store)(nil)).ToInstance(sessionStore)
 	default: //memory
 		sessionStore := memorystore.NewMemoryStore([]byte(m.Secret))
+
 		sessionStore.MaxLength(int(m.StoreLength))
 		sessionStore.MaxAge(int(m.MaxAge))
 		sessionStore.Options.Secure = m.Secure
 		sessionStore.Options.HttpOnly = true
+		sessionStore.Options.Path = m.Path
+
 		injector.Bind((*sessions.Store)(nil)).ToInstance(sessionStore)
 	}
 }
@@ -65,8 +77,10 @@ func (m *Module) DefaultConfig() config.Map {
 		"session.store.length":           float64(1024 * 1024),
 		"session.max.age":                float64(60 * 60 * 24 * 30),
 		"session.cookie.secure":          true,
+		"session.cookie.path":            "/",
 		"session.redis.host":             "redis",
 		"session.redis.password":         "",
 		"session.redis.idle.connections": float64(10),
+		"session.redis.maxAge":           float64(60 * 60 * 24), // 1 day
 	}
 }

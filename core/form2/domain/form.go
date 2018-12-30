d0424307a14e276c7788ac0ad7146670588f1a08
Marko Milojevic <marko.milojevic@aoe.com>
2018-12-30 20:22:54 +0100

    implement form handler

--- core/form2/domain/form.go
+++ core/form2/domain/form.go
@@ -1,9 +1,5 @@
 package domain
 
-import (
-	"net/url"
-)
-
 type (
 	Form struct {
 		//Data  the form Data Struct (Forms DTO)
@@ -12,10 +8,8 @@ type (
 		ValidationInfo ValidationInfo
 		//IsSubmitted  flag if form was submitted and this is the result page
 		IsSubmitted bool
-		//IsSuccess  - if IsValid && IsSubmitted && The planned Action was sucessfull (e.g. register user)
-		IsSuccess bool
-		//OriginalPostValues contain the original posted values
-		OriginalPostValues url.Values
+		//IsValid flag if form was validated successfully
+		IsValid bool
 		//ValidationRules contains map with validation rules for all validatable fields
 		ValidationRules map[string][]ValidationRule
 	}

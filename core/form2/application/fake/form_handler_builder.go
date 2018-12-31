d67dd22cf83271b87b841cc52812c06b1c68053d
Marko Milojevic <marko.milojevic@aoe.com>
2018-12-31 17:59:06 +0100

    fake factory and builder

---
+++ core/form2/application/fake/form_handler_builder.go
@@ -0,0 +1,57 @@
+package application
+
+import (
+	"flamingo.me/flamingo/core/form2/application"
+	"flamingo.me/flamingo/core/form2/domain"
+	"flamingo.me/flamingo/core/form2/domain/mocks"
+)
+
+type (
+	formHandlerBuilderImpl struct {
+		formHandler *mocks.FormHandler
+	}
+)
+
+func (b *formHandlerBuilderImpl) SetNamedFormService(name string) application.FormHandlerBuilder {
+	return b
+}
+
+func (b *formHandlerBuilderImpl) SetFormService(formService interface{}) application.FormHandlerBuilder {
+	return b
+}
+
+func (b *formHandlerBuilderImpl) SetNamedFormDataProvider(name string) application.FormHandlerBuilder {
+	return b
+}
+
+func (b *formHandlerBuilderImpl) SetFormDataProvider(formDataProvider domain.FormDataProvider) application.FormHandlerBuilder {
+	return b
+}
+
+func (b *formHandlerBuilderImpl) SetNamedFormDataDecoder(name string) application.FormHandlerBuilder {
+	return b
+}
+
+func (b *formHandlerBuilderImpl) SetFormDataDecoder(formDataDecoder domain.FormDataDecoder) application.FormHandlerBuilder {
+	return b
+}
+
+func (b *formHandlerBuilderImpl) SetNamedFormDataValidator(name string) application.FormHandlerBuilder {
+	return b
+}
+
+func (b *formHandlerBuilderImpl) SetFormDataValidator(formDataValidator domain.FormDataValidator) application.FormHandlerBuilder {
+	return b
+}
+
+func (b *formHandlerBuilderImpl) AddNamedFormExtension(name string) application.FormHandlerBuilder {
+	return b
+}
+
+func (b *formHandlerBuilderImpl) AddFormExtension(formExtension interface{}) application.FormHandlerBuilder {
+	return b
+}
+
+func (b *formHandlerBuilderImpl) Build() domain.FormHandler {
+	return b.formHandler
+}
---
+++ core/form2/application/fake/form_handler_factory.go
@@ -0,0 +1,37 @@
+package application
+
+import (
+	"flamingo.me/flamingo/core/form2/application"
+	"flamingo.me/flamingo/core/form2/domain"
+	"flamingo.me/flamingo/core/form2/domain/mocks"
+)
+
+type (
+	FormHandlerFactoryImpl struct {
+		formHandler *mocks.FormHandler
+	}
+)
+
+func New(formHandler *mocks.FormHandler) application.FormHandlerFactory {
+	return &FormHandlerFactoryImpl{
+		formHandler: formHandler,
+	}
+}
+
+func (f *FormHandlerFactoryImpl) CreateSimpleFormHandler() domain.FormHandler {
+	return f.formHandler
+}
+
+func (f *FormHandlerFactoryImpl) CreateFormHandlerWithFormService(formService interface{}, formExtensions ...interface{}) domain.FormHandler {
+	return f.formHandler
+}
+
+func (f *FormHandlerFactoryImpl) CreateFormHandlerWithFormServices(formDataProvider domain.FormDataProvider, formDataDecoder domain.FormDataDecoder, formDataValidator domain.FormDataValidator, formExtensions ...interface{}) domain.FormHandler {
+	return f.formHandler
+}
+
+func (f *FormHandlerFactoryImpl) GetFormHandlerBuilder() application.FormHandlerBuilder {
+	return &formHandlerBuilderImpl{
+		formHandler: f.formHandler,
+	}
+}

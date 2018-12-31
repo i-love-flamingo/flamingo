6e62fcad3b6a651c4f32820c176339f54257d4fa
Marko Milojevic <marko.milojevic@aoe.com>
2018-12-31 19:56:14 +0100

    finished code documentation

--- core/form2/application/form_handler_builder.go
+++ core/form2/application/form_handler_builder.go
@@ -6,20 +6,44 @@ import (
 )
 
 type (
+	// FormHandlerBuilder as interface for complex creation of form handler instance
 	FormHandlerBuilder interface {
+		// SetFormService sets form service instance and overrides provider, decoder and validator if
+		// it implements theirs interfaces. If it doesn't implements any of those interfaces it panics.
 		SetFormService(formService interface{}) FormHandlerBuilder
+		// SetNamedFormService sets form service instance by searching named form service provided via dingo injector.
+		// It panics if there is no injected form service with that name.
+		// It overrides provider, decoder and validator if it implements theirs interfaces.
+		// If it doesn't implements any of those interfaces it panics.
 		SetNamedFormService(name string) FormHandlerBuilder
+		// SetFormDataProvider sets form data provider instance and overrides default one.
 		SetFormDataProvider(formDataProvider domain.FormDataProvider) FormHandlerBuilder
+		// SetNamedFormDataProvider sets form data provider by searching named provider provided via dingo injector.
+		// It panics if there is no injected form data provider with that name.
+		// It sets form data provider instance and overrides default one.
 		SetNamedFormDataProvider(name string) FormHandlerBuilder
+		// SetFormDataDecoder sets form data decoder instance and overrides default one.
 		SetFormDataDecoder(formDataDecoder domain.FormDataDecoder) FormHandlerBuilder
+		// SetNamedFormDataDecoder sets form data decoder by searching named decoder provided via dingo injector.
+		// It panics if there is no injected form data decoder with that name.
+		// It sets form data decoder instance and overrides default one.
 		SetNamedFormDataDecoder(name string) FormHandlerBuilder
+		// SetFormDataValidator sets form data validator instance and overrides default one.
 		SetFormDataValidator(formDataValidator domain.FormDataValidator) FormHandlerBuilder
+		// SetNamedFormDataValidator sets form data decoder by searching named decoder validator via dingo injector.
+		// It panics if there is no injected form data validator with that name.
+		// It sets form data validator instance and overrides default one.
 		SetNamedFormDataValidator(name string) FormHandlerBuilder
+		// AddFormExtension adds form extension to the list of form extensions.
 		AddFormExtension(formExtension interface{}) FormHandlerBuilder
+		// AddNamedFormExtension adds form extension by searching named extension via dingo injector.
+		// It panics if there is no injected form extension with that name.
 		AddNamedFormExtension(name string) FormHandlerBuilder
+		// Build creates new instance of FormHandler interface
 		Build() domain.FormHandler
 	}
 
+	// formHandlerBuilderImpl as actual implementation of FormHandlerBuilder interface
 	formHandlerBuilderImpl struct {
 		namedFormServices        []domain.FormServiceWithName
 		namedFormDataProviders   []domain.FormDataProviderWithName
@@ -38,6 +62,10 @@ type (
 	}
 )
 
+// SetNamedFormService sets form service instance by searching named form service provided via dingo injector.
+// It panics if there is no injected form service with that name.
+// It overrides provider, decoder and validator if it implements theirs interfaces.
+// If it doesn't implements any of those interfaces it panics.
 func (b *formHandlerBuilderImpl) SetNamedFormService(name string) FormHandlerBuilder {
 	for _, service := range b.namedFormServices {
 		if name == service.Name() {
@@ -48,6 +76,8 @@ func (b *formHandlerBuilderImpl) SetNamedFormService(name string) FormHandlerBui
 	panic(fmt.Sprintf(`there is no FormService with name "%s"`, name))
 }
 
+// SetFormService sets form service instance and overrides provider, decoder and validator if
+// it implements theirs interfaces. If it doesn't implements any of those interfaces it panics.
 func (b *formHandlerBuilderImpl) SetFormService(formService interface{}) FormHandlerBuilder {
 	set := false
 	if provider, ok := formService.(domain.FormDataProvider); ok {
@@ -68,6 +98,9 @@ func (b *formHandlerBuilderImpl) SetFormService(formService interface{}) FormHan
 	return b
 }
 
+// SetNamedFormDataProvider sets form data provider by searching named provider provided via dingo injector.
+// It panics if there is no injected form data provider with that name.
+// It sets form data provider instance and overrides default one.
 func (b *formHandlerBuilderImpl) SetNamedFormDataProvider(name string) FormHandlerBuilder {
 	for _, provider := range b.namedFormDataProviders {
 		if name == provider.Name() {
@@ -78,12 +111,16 @@ func (b *formHandlerBuilderImpl) SetNamedFormDataProvider(name string) FormHandl
 	panic(fmt.Sprintf(`there is no FormDataProvider with name "%s"`, name))
 }
 
+// SetFormDataProvider sets form data provider instance and overrides default one.
 func (b *formHandlerBuilderImpl) SetFormDataProvider(formDataProvider domain.FormDataProvider) FormHandlerBuilder {
 	b.formDataProvider = formDataProvider
 
 	return b
 }
 
+// SetNamedFormDataDecoder sets form data decoder by searching named decoder provided via dingo injector.
+// It panics if there is no injected form data decoder with that name.
+// It sets form data decoder instance and overrides default one.
 func (b *formHandlerBuilderImpl) SetNamedFormDataDecoder(name string) FormHandlerBuilder {
 	for _, decoder := range b.namedFormDataDecoders {
 		if name == decoder.Name() {
@@ -94,12 +131,16 @@ func (b *formHandlerBuilderImpl) SetNamedFormDataDecoder(name string) FormHandle
 	panic(fmt.Sprintf(`there is no FormDataDecoder with name "%s"`, name))
 }
 
+// SetFormDataDecoder sets form data decoder instance and overrides default one.
 func (b *formHandlerBuilderImpl) SetFormDataDecoder(formDataDecoder domain.FormDataDecoder) FormHandlerBuilder {
 	b.formDataDecoder = formDataDecoder
 
 	return b
 }
 
+// SetNamedFormDataValidator sets form data decoder by searching named decoder validator via dingo injector.
+// It panics if there is no injected form data validator with that name.
+// It sets form data validator instance and overrides default one.
 func (b *formHandlerBuilderImpl) SetNamedFormDataValidator(name string) FormHandlerBuilder {
 	for _, validator := range b.namedFormDataValidators {
 		if name == validator.Name() {
@@ -110,12 +151,15 @@ func (b *formHandlerBuilderImpl) SetNamedFormDataValidator(name string) FormHand
 	panic(fmt.Sprintf(`there is no FormDataValidator with name "%s"`, name))
 }
 
+// SetFormDataValidator sets form data validator instance and overrides default one.
 func (b *formHandlerBuilderImpl) SetFormDataValidator(formDataValidator domain.FormDataValidator) FormHandlerBuilder {
 	b.formDataValidator = formDataValidator
 
 	return b
 }
 
+// AddNamedFormExtension adds form extension by searching named extension via dingo injector.
+// It panics if there is no injected form extension with that name.
 func (b *formHandlerBuilderImpl) AddNamedFormExtension(name string) FormHandlerBuilder {
 	for _, extension := range b.namedFormExtensions {
 		if name == extension.Name() {
@@ -126,6 +170,7 @@ func (b *formHandlerBuilderImpl) AddNamedFormExtension(name string) FormHandlerB
 	panic(fmt.Sprintf(`there is no FormExtension with name "%s"`, name))
 }
 
+// AddFormExtension adds form extension to the list of form extensions.
 func (b *formHandlerBuilderImpl) AddFormExtension(formExtension interface{}) FormHandlerBuilder {
 	implements := false
 	if _, ok := formExtension.(domain.FormDataProvider); ok {
@@ -146,6 +191,7 @@ func (b *formHandlerBuilderImpl) AddFormExtension(formExtension interface{}) For
 	return b
 }
 
+// Build creates new instance of FormHandler interface
 func (b *formHandlerBuilderImpl) Build() domain.FormHandler {
 	formDataProvider := b.formDataProvider
 	if formDataProvider == nil {
--- core/form2/domain/form.go
+++ core/form2/domain/form.go
@@ -1,18 +1,20 @@
 package domain
 
 type (
+	// Form as struct for storing form processing results
 	Form struct {
-		//Data  the form Data Struct (Forms DTO)
+		// Data  the form Data Struct (Forms DTO)
 		Data interface{}
-		//ValidationInfo for the form
+		// ValidationInfo for the form
 		ValidationInfo ValidationInfo
-		//submitted  flag if form was submitted and this is the result page
+		// submitted  flag if form was submitted and this is the result page
 		submitted bool
-		//validationRules contains map with validation rules for all validatable fields
+		// validationRules contains map with validation rules for all validatable fields
 		validationRules map[string][]ValidationRule
 	}
 )
 
+// NewForm returns new instance of Form struct
 func NewForm(submitted bool, validationRules map[string][]ValidationRule) Form {
 	return Form{
 		submitted:       submitted,
@@ -20,30 +22,37 @@ func NewForm(submitted bool, validationRules map[string][]ValidationRule) Form {
 	}
 }
 
+// IsValidAndSubmitted defines if form data is valid and form is submitted
 func (f Form) IsValidAndSubmitted() bool {
 	return f.IsValid() && f.IsSubmitted()
 }
 
+// IsValid defines if form data is valid
 func (f Form) IsValid() bool {
 	return f.ValidationInfo.IsValid()
 }
 
+// IsSubmitted defines if form is submitted
 func (f Form) IsSubmitted() bool {
 	return f.submitted
 }
 
+// HasErrorsForField method which defines if there is any field validations error for specific field
 func (f Form) HasErrorForField(name string) bool {
 	return f.ValidationInfo.HasErrorsForField(name)
 }
 
+// HasAnyFieldErrors method which defines if there is any field validations error for any field
 func (f Form) HasAnyFieldErrors() bool {
 	return f.ValidationInfo.HasAnyFieldErrors()
 }
 
+// HasGeneralErrors method which defines if there is any general validations error
 func (f Form) HasGeneralErrors() bool {
 	return f.ValidationInfo.HasGeneralErrors()
 }
 
+// GetFieldErrors method which returns list of all general validation errors for specific field
 func (f Form) GetErrorsForField(name string) []Error {
 	return f.ValidationInfo.GetErrorsForField(name)
 }

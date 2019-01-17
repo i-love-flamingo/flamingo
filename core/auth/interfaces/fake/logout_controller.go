c49a952efd7502a052db328e6727628b2d8a4ef7
Marko Milojevic <marko.milojevic@aoe.com>
2019-01-17 22:16:23 +0100

    added event publisher for fake logout controller

--- core/auth/interfaces/fake/logout_controller.go
+++ core/auth/interfaces/fake/logout_controller.go
@@ -5,6 +5,7 @@ import (
 
 	"flamingo.me/flamingo/core/auth/application"
 	"flamingo.me/flamingo/core/auth/application/fake"
+	"flamingo.me/flamingo/core/auth/domain"
 	"flamingo.me/flamingo/framework/web"
 	"flamingo.me/flamingo/framework/web/responder"
 )
@@ -13,20 +14,26 @@ type (
 	LogoutController struct {
 		responder.RedirectAware
 
-		authManager *application.AuthManager
+		authManager    *application.AuthManager
+		eventPublisher *application.EventPublisher
 	}
 )
 
 func (l *LogoutController) Inject(
 	redirectAware responder.RedirectAware,
 	authManager *application.AuthManager,
+	eventPublisher *application.EventPublisher,
 ) {
 	l.RedirectAware = redirectAware
 	l.authManager = authManager
+	l.eventPublisher = eventPublisher
 }
 
 func (l *LogoutController) Get(ctx context.Context, request *web.Request) web.Response {
 	request.Session().Delete(fake.UserSessionKey)
+	l.eventPublisher.PublishLogoutEvent(ctx, &domain.LogoutEvent{
+		Session: request.Session().G(),
+	})
 
 	redirectUrl, _ := l.authManager.URL(ctx, "")
 

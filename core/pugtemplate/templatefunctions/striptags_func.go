f13ec2c2cc7549a4e2ee697896fda121ebcc24be
mathias.schopmans <mathias.schopmans@aoe.com>
2019-03-21 17:50:30 +0100

    193974: Add optional parameter to stripTags() template function
    This allows developers to output html safely as it will only output specified tags and their whitelisted attributes.
    
    As an example you can create a new list in `config.yml` and use it in your template with the config() function:
    
    ```yml
    allowedTags:
     - "h2"
     - "h3"
     - "p"
     - "a(href target rel)"
     - "img(src alt)"
    
    ```yml
    
    ```pug
    .productDescription!= stripTags(description, config('allowedTags'))
    ```

--- core/pugtemplate/templatefunctions/striptags_func.go
+++ core/pugtemplate/templatefunctions/striptags_func.go
@@ -1,42 +1,113 @@
 package templatefunctions
 
 import (
+	"flamingo.me/flamingo/framework/config"
+	"regexp"
 	"strings"
 
 	"golang.org/x/net/html"
 )
 
 type (
-	StriptagsFunc struct{}
+	StriptagsFunc     struct{}
+	AllowedAttributes map[string]bool
+	AllowedTags       []AllowedTag
+	AllowedTag        struct {
+		name       string
+		attributes AllowedAttributes
+	}
 )
+
+var nameRe = regexp.MustCompile(`[a-z0-9\-]+`)
+var attributesRe = regexp.MustCompile(`[a-z0-9]+([a-z]+)`)
+
+func createTag(definition string) AllowedTag {
+	definition = strings.ToLower(definition)
+	attributes := make(AllowedAttributes)
+	for _, attr := range attributesRe.FindAllString(definition, -1) {
+		attributes[attr] = true
+	}
+
+	return AllowedTag{
+		nameRe.FindString(definition),
+		attributes,
+	}
+}
+
+func (at AllowedTags) Find(tagName string) *AllowedTag {
+	for _, tag := range at {
+		if tag.name == tagName {
+			return &tag
+		}
+	}
+	return nil
+}
+
+func (at AllowedTags) Contains(tagName string) bool {
+	if tag := at.Find(tagName); tag != nil {
+		return true
+	}
+	return false
+}
 
 // Func as implementation of debug method
 func (df StriptagsFunc) Func() interface{} {
-	return func(htmlString string) string {
+	return func(htmlString string, allowedTagsConfig ...config.Slice) string {
 		doc, err := html.ParseFragment(strings.NewReader(htmlString), nil)
 		if err != nil {
 			return ""
 		}
 
+		var allowedTags AllowedTags
+		if len(allowedTagsConfig) == 1 {
+			for _, item := range allowedTagsConfig[0] {
+				if definition, ok := item.(string); ok {
+					allowedTags = append(allowedTags, createTag(definition))
+				}
+			}
+		}
+
 		res := ""
 		for _, n := range doc {
-			res += removeTags(n)
+			res += cleanTags(n, allowedTags)
 		}
 		return res
 	}
 }
 
-func removeTags(n *html.Node) string {
+func cleanTags(n *html.Node, allowedTags AllowedTags) string {
 	res := ""
+
+	if n.Type == html.ElementNode && allowedTags.Contains(n.Data) {
+		res += "<"
+		res += n.Data
+		res += getAllowedAttributes(n.Attr, allowedTags.Find(n.Data).attributes)
+		res += ">"
+	}
 
 	if n.Type == html.TextNode {
 		res += n.Data
 	}
+
 	if n.FirstChild != nil {
 		for c := n.FirstChild; c != nil; c = c.NextSibling {
-			res += removeTags(c)
+			res += cleanTags(c, allowedTags)
 		}
 	}
 
+	if n.Type == html.ElementNode && allowedTags.Contains(n.Data) {
+		res += "</" + n.Data + ">"
+	}
+
+	return res
+}
+
+func getAllowedAttributes(attributes []html.Attribute, allowedAttributes AllowedAttributes) string {
+	res := ""
+	for _, attr := range attributes {
+		if allowedAttributes[attr.Key] {
+			res += " " + attr.Key + "=\"" + html.EscapeString(attr.Val) + "\""
+		}
+	}
 	return res
 }
---
+++ core/pugtemplate/templatefunctions/striptags_func_test.go
@@ -0,0 +1,39 @@
+package templatefunctions
+
+import (
+	"flamingo.me/flamingo/framework/config"
+	"flamingo.me/flamingo/framework/template"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestStriptagsFunc(t *testing.T) {
+
+	var striptagsFunc template.Func = new(StriptagsFunc)
+	striptags := striptagsFunc.Func().(func(htmlString string, allowedTagsConfig ...config.Slice) string)
+
+	// basic test without parameter
+	// should remove all tags and keep only text
+	assert.Equal(t, "should be ok", striptags("should be ok"))
+	assert.Equal(t, "Headline\nHello world", striptags("<h1>Headline</h1>\n<p>Hello world</p>"))
+
+	// advanced test with parameter
+	// should only keep wanted tags
+	var res string
+	res = striptags("<h1>Hello</h1>", config.Slice{"h1", "h2", "h3", "p"})
+	assert.Equal(t, "<h1>Hello</h1>", res)
+
+	res = striptags("<h1>Remove Headline</h1>", config.Slice{"p"})
+	assert.Equal(t, "Remove Headline", res)
+
+	res = striptags("<h1>Remove Headline</h1><p>Keep paragraphs</p>", config.Slice{"p"})
+	assert.Equal(t, "Remove Headline<p>Keep paragraphs</p>", res)
+
+	// remove invalid attributes and tags
+	res = striptags("<h1 style=\"font-size: 500px\">Remove Scripts</h1><script src=\"http://miner.tld/x.js\">", config.Slice{"h1"})
+	assert.Equal(t, "<h1>Remove Scripts</h1>", res, "should remove invalid attributes")
+
+	//keep allowed attributes
+	res = striptags("<p>I'm a paragraph containing a <a href=\"http://tld.com\">link</a></p>", config.Slice{"p", "a(href link rel)"})
+	assert.Equal(t, "<p>I'm a paragraph containing a <a href=\"http://tld.com\">link</a></p>", res, "should remove invalid attributes")
+}

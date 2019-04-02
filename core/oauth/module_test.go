48265ccf1d640322786f3269bfc2fc130114d013
Daniel Pötzinger <daniel.poetzinger@aoe.com>
2019-04-02 22:02:13 +0200

    rename auth package to oauth

--- core/oauth/module_test.go
+++ core/oauth/module_test.go
@@ -1,21 +1,21 @@
-package auth_test
+package oauth_test
 
 import (
 	"testing"
 
 	"flamingo.me/dingo"
-	auth "flamingo.me/flamingo/v3/core/oauth"
+	"flamingo.me/flamingo/v3/core/oauth"
 	"flamingo.me/flamingo/v3/framework/config"
 )
 
 func TestModule_Configure(t *testing.T) {
 	cfgModule := &config.Module{
-		Map: new(auth.Module).DefaultConfig(),
+		Map: new(oauth.Module).DefaultConfig(),
 	}
 
 	cfgModule.Map["session.backend"] = ""
 
-	if err := dingo.TryModule(cfgModule, new(auth.Module)); err != nil {
+	if err := dingo.TryModule(cfgModule, new(oauth.Module)); err != nil {
 		t.Error(err)
 	}
 }

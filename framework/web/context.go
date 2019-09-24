9f463a77986a6ed4e44e085031c888400e00be7f
Daniel Pötzinger <daniel.poetzinger@aoe.com>
2019-09-24 18:26:35 +0200

    RunWithDetachedContext - avoid errors if request has no session

--- framework/web/context.go
+++ framework/web/context.go
@@ -9,9 +9,12 @@ import (
 // RunWithDetachedContext returns a context which is detached from the original deadlines, timeouts & co
 func RunWithDetachedContext(origCtx context.Context, fnc func(ctx context.Context)) {
 	request := RequestFromContext(origCtx)
-
+	session  := SessionFromContext(origCtx)
+	if request != nil && session == nil{
+		session = request.Session()
+	}
 	ctx := ContextWithRequest(context.Background(), request)
-	ctx = ContextWithSession(ctx, request.Session())
+	ctx = ContextWithSession(ctx, session)
 
 	ctx, span := trace.StartSpanWithRemoteParent(ctx, "flamingo/detachedContext", trace.FromContext(origCtx).SpanContext())
 	defer span.End()

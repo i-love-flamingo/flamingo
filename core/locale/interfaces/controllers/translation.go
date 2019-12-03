d5f6ad1e35851e3161130b80d8a7480d4172038f
Deniz Dagtekin <deniz.dagtekin@aoe.com>
2019-12-03 13:44:56 +0100

    Add caching for the TranslationService api

--- core/locale/interfaces/controllers/translation.go
+++ core/locale/interfaces/controllers/translation.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"flamingo.me/flamingo/v3/core/locale/application"
 	"flamingo.me/flamingo/v3/framework/web"
+	"sync"
 )
 
 type (
@@ -11,6 +12,8 @@ type (
 	TranslationController struct {
 		responder    *web.Responder
 		labelService *application.LabelService
+		labelCache   []TranslationJSON
+		lock         sync.RWMutex
 	}
 
 	// TranslationJSON helper struct to map the result
@@ -31,15 +34,25 @@ func (c *TranslationController) Inject(
 
 // GetAllTranslations controller for TranslationController
 func (c *TranslationController) GetAllTranslations(ctx context.Context, r *web.Request) web.Result {
-	translations := []TranslationJSON{}
-	l := c.labelService.AllLabels()
+	c.lock.RLock()
+	defer c.lock.RUnlock()
+	if c.labelCache == nil {
+		c.lock.RUnlock()
+		c.lock.Lock()
 
-	for _, la := range l {
-		translations = append(translations, TranslationJSON{
-			Key:         la.GetKey(),
-			Translation: la.String(),
-		})
+		l := c.labelService.AllLabels()
+		translations := make([]TranslationJSON, 0, len(l))
+
+		for _, la := range l {
+			translations = append(translations, TranslationJSON{
+				Key:         la.GetKey(),
+				Translation: la.String(),
+			})
+		}
+		c.labelCache = translations
+		c.lock.Unlock()
+		c.lock.RLock()
 	}
 
-	return c.responder.Data(translations)
+	return c.responder.Data(c.labelCache)
 }

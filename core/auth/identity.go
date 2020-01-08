89916ed0ae39b78c394ccce7061efe99db5ae23a
Bastian Ike <bastian.ike@aoe.com>
2020-01-08 15:35:49 +0100

    openid login and logout

--- core/auth/identity.go
+++ core/auth/identity.go
@@ -1,5 +1,12 @@
 package auth
 
+import (
+	"context"
+
+	"flamingo.me/flamingo/v3/core/security/domain"
+	"flamingo.me/flamingo/v3/framework/web"
+)
+
 type (
 	// Identity donates an authentication object which at least identifies the authenticated subject
 	Identity interface {
@@ -7,8 +14,39 @@ type (
 		Broker() string
 	}
 
-	// HasRoles adds the ability to provide roles to an identity
-	HasRoles interface {
+	// hasRoles adds the ability to provide roles to an identity
+	hasRoles interface {
 		Roles() []string
 	}
+
+	securityRoleProvider struct {
+		service *WebIdentityService
+	}
 )
+
+func (p *securityRoleProvider) Inject(service *WebIdentityService) {
+	p.service = service
+}
+
+func (p *securityRoleProvider) All(ctx context.Context, _ *web.Session) []domain.Role {
+	request := web.RequestFromContext(ctx)
+	if request == nil {
+		return nil
+	}
+
+	var roles []domain.Role
+	var identified bool
+	for _, identity := range p.service.IdentifyAll(ctx, request) {
+		_ = identity
+		identified = true
+		//if roler, ok := identity.(hasRoles); ok {
+		//roles = append(roles, roler.Roles()...)
+		//}
+	}
+
+	if identified {
+		roles = append(roles, domain.StringRole(domain.PermissionAuthorized))
+	}
+
+	return roles
+}

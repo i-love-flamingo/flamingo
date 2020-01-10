5cef22f803617aabb60e96be80ef8b1bc936e1c9
Bastian Ike <bastian.ike@aoe.com>
2020-01-10 10:37:58 +0100

    fix error template typo

--- framework/module.go
+++ framework/module.go
@@ -101,10 +101,10 @@ flamingo: {
 		path?: string
 	}
 	template: {
-		err403: string | *"err/403"
-		err404: string | *"err/404"
-		errWithCode: string | *"err/withCode"
-		err503: string | *"err/503"
+		err403: string | *"error/403"
+		err404: string | *"error/404"
+		errWithCode: string | *"error/withCode"
+		err503: string | *"error/503"
 	}
 	session: name: string | *"flamingo"
 }

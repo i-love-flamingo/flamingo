5dc46b4c16a483e1bfbe6f1496a270a0f2b068e6
Thomas Neumann <thomas.neumann@aoe.com>
2020-02-06 00:12:47 +0100

    Fix module test

--- core/auth/fake/module_test.go
+++ core/auth/fake/module_test.go
@@ -10,26 +10,22 @@ func TestModule(t *testing.T) {
 	t.Parallel()
 	c := config.Map{
 		"flamingo.debug.mode": true,
-		"core": config.Map{
-			"auth": config.Map{
-				"fake": config.Map{
-					"loginTemplate": "testTemplateName",
-					"userConfig": config.Map{
-						"testUserA": config.Map{
-							"password": "testUserAPassword",
-						},
-						"testUserB": config.Map{
-							"password": "testUserBPassword",
-							"otp":      "testUserBotp",
-						},
-					},
-					"validatePassword": true,
-					"validateOtp":      true,
-					"usernameFieldId":  "username",
-					"passwordFieldId":  "password",
-					"otpFieldId":       "otp",
+		"fake": config.Map{
+			"loginTemplate": "testTemplateName",
+			"userConfig": config.Map{
+				"testUserA": config.Map{
+					"password": "testUserAPassword",
+				},
+				"testUserB": config.Map{
+					"password": "testUserBPassword",
+					"otp":      "testUserBotp",
 				},
 			},
+			"validatePassword": true,
+			"validateOtp":      true,
+			"usernameFieldId":  "username",
+			"passwordFieldId":  "password",
+			"otpFieldId":       "otp",
 		},
 	}
 	if err := config.TryModules(c, new(Module)); err != nil {

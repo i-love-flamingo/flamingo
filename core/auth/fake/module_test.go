0a25ddb3361b3efe4bea2df380991263625ce6b3
chetan.thapliyal <chetan.thapliyal@aoe.com>
2020-02-25 18:47:02 +0100

    Fix fake adapter unit test

--- core/auth/fake/module_test.go
+++ core/auth/fake/module_test.go
@@ -10,20 +10,23 @@ func TestModule(t *testing.T) {
 	t.Parallel()
 	c := config.Map{
 		"flamingo.debug.mode": true,
-		"core.auth.fake": config.Map{
-			"broker":        "fakeBroker",
-			"loginTemplate": "testTemplateName",
-			"userConfig": config.Map{
-				"testUserA": config.Map{
-					"password": "testUserAPassword",
+		"core.auth.web.broker": []config.Map{
+			{
+				"typ":           "fake",
+				"broker":        "fakeBroker",
+				"loginTemplate": "testTemplateName",
+				"userConfig": config.Map{
+					"testUserA": config.Map{
+						"password": "testUserAPassword",
+					},
+					"testUserB": config.Map{
+						"password": "testUserBPassword",
+					},
 				},
-				"testUserB": config.Map{
-					"password": "testUserBPassword",
-				},
+				"validatePassword": true,
+				"usernameFieldId":  "username",
+				"passwordFieldId":  "password",
 			},
-			"validatePassword": true,
-			"usernameFieldId":  "username",
-			"passwordFieldId":  "password",
 		},
 	}
 	if err := config.TryModules(c, new(Module)); err != nil {

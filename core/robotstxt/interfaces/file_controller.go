c6c535aee5bff8afe0e753ffd81345472dc963dc
Thorsten Essig <thorsten.essig@omnevo.net>
2021-02-11 10:54:36 +0100

    core/robotstxt: clean up after code review

--- core/robotstxt/interfaces/file_controller.go
+++ core/robotstxt/interfaces/file_controller.go
@@ -3,22 +3,16 @@ package interfaces
 import (
 	"bytes"
 	"context"
-	"flamingo.me/flamingo/v3/framework/flamingo"
-	"flamingo.me/flamingo/v3/framework/web"
 	"io/ioutil"
 	"net/http"
+
+	"flamingo.me/flamingo/v3/framework/flamingo"
+	"flamingo.me/flamingo/v3/framework/web"
 )
 
 type (
-	// FileControllerInterface is the callback HTTP action provider
-	FileControllerInterface interface {
-		GetRobotsTxt(context.Context, *web.Request) web.Result
-		GetSecurityTxt(context.Context, *web.Request) web.Result
-		GetHumansTxt(context.Context, *web.Request) web.Result
-	}
-
-	// DefaultFileControllerInterface implementing FileControllerInterface
-	DefaultFileControllerInterface struct {
+	// FileController implementing FileController
+	FileController struct {
 		responder           *web.Responder
 		logger              flamingo.Logger
 		robotsTxtFilepath   string
@@ -27,12 +21,8 @@ type (
 	}
 )
 
-var (
-	_ FileControllerInterface = new(DefaultFileControllerInterface)
-)
-
 // Inject configuration
-func (d *DefaultFileControllerInterface) Inject(
+func (d *FileController) Inject(
 	responder *web.Responder,
 	logger flamingo.Logger,
 	config *struct {
@@ -51,22 +41,22 @@ func (d *DefaultFileControllerInterface) Inject(
 }
 
 // GetRobotsTxt returns /robots.txt
-func (d *DefaultFileControllerInterface) GetRobotsTxt(ctx context.Context, _ *web.Request) web.Result {
+func (d *FileController) GetRobotsTxt(ctx context.Context, _ *web.Request) web.Result {
 	return d.serveFile(ctx, d.robotsTxtFilepath)
 }
 
 // GetSecurityTxt returns /.well-known/security.txt
-func (d *DefaultFileControllerInterface) GetSecurityTxt(ctx context.Context, _ *web.Request) web.Result {
+func (d *FileController) GetSecurityTxt(ctx context.Context, _ *web.Request) web.Result {
 	return d.serveFile(ctx, d.securityTxtFilepath)
 }
 
 // GetHumansTxt returns /humans.txt
-func (d *DefaultFileControllerInterface) GetHumansTxt(ctx context.Context, _ *web.Request) web.Result {
+func (d *FileController) GetHumansTxt(ctx context.Context, _ *web.Request) web.Result {
 	return d.serveFile(ctx, d.humansTxtFilepath)
 }
 
-func (d *DefaultFileControllerInterface) serveFile(ctx context.Context, filePath string) web.Result {
-	fileContent, err := d.readFile(filePath)
+func (d *FileController) serveFile(ctx context.Context, filePath string) web.Result {
+	fileContent, err := ioutil.ReadFile(filePath)
 	if err != nil {
 		d.logger.WithContext(ctx).Error(err)
 
@@ -74,13 +64,4 @@ func (d *DefaultFileControllerInterface) serveFile(ctx context.Context, filePath
 	}
 
 	return d.responder.HTTP(http.StatusOK, bytes.NewReader(fileContent))
-}
-
-func (d *DefaultFileControllerInterface) readFile(filePath string) ([]byte, error) {
-	b, err := ioutil.ReadFile(filePath)
-	if err != nil {
-		return nil, err
-	}
-
-	return b, nil
 }
--- core/robotstxt/module.go
+++ core/robotstxt/module.go
@@ -1,25 +1,35 @@
 package robotstxt
 
 import (
+	"net/http"
+
 	"flamingo.me/dingo"
+
 	"flamingo.me/flamingo/v3/core/robotstxt/interfaces"
 	"flamingo.me/flamingo/v3/framework/web"
 )
 
 type (
 	// Module for robotstxt
-	Module struct{}
+	Module struct {
+		DefaultMux *http.ServeMux `inject:",optional"`
+		Filepath   string         `inject:"config:core.robotstxt.filepath"`
+	}
 
 	routes struct {
 		securityTxtActivated bool
 		humansTxtActivated   bool
-		files                interfaces.FileControllerInterface
+		files                *interfaces.FileController
 	}
 )
 
 // Configure DI
 func (m *Module) Configure(injector *dingo.Injector) {
-	injector.Bind(new(interfaces.FileControllerInterface)).To(interfaces.DefaultFileControllerInterface{})
+	if m.DefaultMux != nil {
+		m.DefaultMux.HandleFunc("/robots.txt", func(rw http.ResponseWriter, req *http.Request) {
+			http.ServeFile(rw, req, m.Filepath)
+		})
+	}
 	web.BindRoutes(injector, new(routes))
 }
 
@@ -27,10 +37,14 @@ func (m *Module) Configure(injector *dingo.Injector) {
 func (*Module) CueConfig() string {
 	return `
 core: robotstxt: filepath: string | *"frontend/robots.txt"
-core: securitytxt: enabled:  bool | *false
-core: securitytxt: filepath: string | *"frontend/security.txt"
-core: humanstxt: enabled:  bool | *false
-core: humanstxt: filepath: string | *"frontend/humans.txt"
+core: securitytxt: {
+	enabled:  bool | *false
+	filepath: string | *"frontend/security.txt"
+}
+core: humanstxt: {
+	enabled:  bool | *false
+	filepath: string | *"frontend/humans.txt"
+}
 `
 }
 
@@ -41,38 +55,31 @@ func (*Module) FlamingoLegacyConfigAlias() map[string]string {
 
 // Inject routes dependencies
 func (r *routes) Inject(
-	config *struct {
+	files *interfaces.FileController,
+	cfg *struct {
 		SecurityTxtActivated bool `inject:"config:core.securitytxt.enabled,optional"`
 		HumansTxtActivated   bool `inject:"config:core.humanstxt.enabled,optional"`
 	},
-	files interfaces.FileControllerInterface,
 ) {
-	r.securityTxtActivated = config.SecurityTxtActivated
-	r.humansTxtActivated = config.HumansTxtActivated
+	if cfg != nil {
+		r.securityTxtActivated = cfg.SecurityTxtActivated
+		r.humansTxtActivated = cfg.HumansTxtActivated
+	}
 	r.files = files
 }
 
 // Routes module
 func (r *routes) Routes(registry *web.RouterRegistry) {
 	registry.HandleGet("robotstxt.robotstxt", r.files.GetRobotsTxt)
-	_, err := registry.Route("/robots.txt", "robotstxt.robotstxt")
-	if err != nil {
-		panic(err)
-	}
+	registry.MustRoute("/robots.txt", "robotstxt.robotstxt")
 
 	if r.securityTxtActivated {
 		registry.HandleGet("robotstxt.securitytxt", r.files.GetSecurityTxt)
-		_, err := registry.Route("/.well-known/security.txt", "robotstxt.securitytxt")
-		if err != nil {
-			panic(err)
-		}
+		registry.MustRoute("/.well-known/security.txt", "robotstxt.securitytxt")
 	}
 
 	if r.humansTxtActivated {
 		registry.HandleGet("robotstxt.humanstxt", r.files.GetHumansTxt)
-		_, err := registry.Route("/humans.txt", "robotstxt.humanstxt")
-		if err != nil {
-			panic(err)
-		}
+		registry.MustRoute("/humans.txt", "robotstxt.humanstxt")
 	}
 }

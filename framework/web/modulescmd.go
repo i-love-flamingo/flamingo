599954fe3d496fe0cd398cdb7199df30dd5c3645
Ivan Maidurov <ivan.maidurov@omnevo.net>
2022-07-04 11:26:15 +0200

    module dump command extended to have print childs

--- framework/web/modulescmd.go
+++ framework/web/modulescmd.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	"reflect"
 
+	"flamingo.me/dingo"
 	"github.com/spf13/cobra"
 
 	"flamingo.me/flamingo/v3/framework/config"
 )
+
+const showAllDuplicatesArg = "-a"
 
 // ModulesCmd for debugging the router configuration
 func ModulesCmd(area *config.Area) *cobra.Command {
@@ -15,9 +18,14 @@ func ModulesCmd(area *config.Area) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "modules",
 		Short: "Modules dump",
-		Run: func(_ *cobra.Command, _ []string) {
+		Run: func(_ *cobra.Command, args []string) {
 
-			dumpModules(area)
+			argsMap := make(map[string]struct{})
+			for _, arg := range args {
+				argsMap[arg] = struct{}{}
+			}
+
+			dumpModules(area, argsMap)
 
 		},
 	}
@@ -25,20 +33,49 @@ func ModulesCmd(area *config.Area) *cobra.Command {
 	return cmd
 }
 
-func dumpModules(area *config.Area) {
+func dumpModules(area *config.Area, argsMap map[string]struct{}) {
 	if area == nil {
 		return
 	}
+
+	_, printDuplicatedModules := argsMap[showAllDuplicatesArg]
+
 	fmt.Println()
 	fmt.Println("****************************************************************************")
-	fmt.Println("Modules Names:               ")
+	fmt.Println("Root Area Modules:               ")
 	fmt.Println("****************************************************************************")
 
+	registry := make(map[string]struct{})
+
 	for _, module := range area.Modules {
-		moduleType := reflect.TypeOf(module)
-		if moduleType.Kind() == reflect.Ptr {
-			fmt.Print("*")
-		}
-		fmt.Printf("%s.%s\n", moduleType.Elem().PkgPath(), moduleType.Elem().Name())
+		moduleName := getModuleName(module)
+		registry[moduleName] = struct{}{}
+		fmt.Print(moduleName)
 	}
+
+	for _, childArea := range area.Childs {
+		fmt.Println()
+		fmt.Println("****************************************************************************")
+		fmt.Printf("Child Area: %s\n", childArea.Name)
+		fmt.Println("****************************************************************************")
+
+		for _, module := range childArea.Modules {
+			moduleName := getModuleName(module)
+			_, foundDuplicate := registry[moduleName]
+			printModuleName := !foundDuplicate || printDuplicatedModules
+			if printModuleName {
+				fmt.Print(moduleName)
+			}
+		}
+	}
+	fmt.Println()
+}
+
+func getModuleName(module dingo.Module) string {
+	moduleType := reflect.TypeOf(module)
+	toRemember := ""
+	if moduleType.Kind() == reflect.Ptr {
+		toRemember = toRemember + "*"
+	}
+	return fmt.Sprintf("%s%s.%s\n", toRemember, moduleType.Elem().PkgPath(), moduleType.Elem().Name())
 }
--- framework/web/modulescmd_test.go
+++ framework/web/modulescmd_test.go
@@ -14,23 +14,62 @@ type (
 	testModule1 struct{}
 	testModule2 struct{}
 	testModule3 struct{}
+	testModule4 struct{}
 )
 
 func (m1 *testModule1) Configure(_ *dingo.Injector) {}
 func (m2 *testModule2) Configure(_ *dingo.Injector) {}
 func (m3 *testModule3) Configure(_ *dingo.Injector) {}
+func (m3 *testModule4) Configure(_ *dingo.Injector) {}
 
 func TestModulesCmd_Print(t *testing.T) {
-	t.Run("", func(t *testing.T) {
+	t.Run("Visual test: print modules without duplicates", func(t *testing.T) {
 		modules := []dingo.Module{
 			new(testModule1),
 			new(testModule2),
 			new(testModule3),
 		}
+
+		childModules := []dingo.Module{
+			new(testModule1),
+			new(testModule2),
+			new(testModule3),
+			new(testModule4),
+		}
+
 		testArea := config.NewArea("testArea", modules)
+		childArea := config.NewArea("childTestArea", childModules)
+		testArea.Childs = []*config.Area{
+			childArea,
+		}
 
 		function := web.ModulesCmd(testArea).Run
 		assert.NotNil(t, function)
 		function(nil, nil)
+	})
+
+	t.Run("Visual test: print modules with duplicates", func(t *testing.T) {
+		modules := []dingo.Module{
+			new(testModule1),
+			new(testModule2),
+			new(testModule3),
+		}
+
+		childModules := []dingo.Module{
+			new(testModule1),
+			new(testModule2),
+			new(testModule3),
+			new(testModule4),
+		}
+
+		testArea := config.NewArea("testArea", modules)
+		childArea := config.NewArea("childTestArea", childModules)
+		testArea.Childs = []*config.Area{
+			childArea,
+		}
+
+		function := web.ModulesCmd(testArea).Run
+		assert.NotNil(t, function)
+		function(nil, []string{"-a"})
 	})
 }

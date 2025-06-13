60afe6947179aef4395d7d012c74d2157d1c0c6e
Thorsten Essig <tessig@users.noreply.github.com>
2025-06-13 10:52:07 +0200

    chore(deps): update module github.com/vektra/mockery/v2 to v3 (#508)

--- app.go
+++ app.go
@@ -28,7 +28,7 @@ import (
 	"flamingo.me/flamingo/v3/framework/web"
 )
 
-//go:generate go run github.com/vektra/mockery/v2@v2.52.2
+//go:generate go tool mockery
 
 const (
 	// FailedShutdownExitCode is returned when application cannot accomplish graceful shutdown
